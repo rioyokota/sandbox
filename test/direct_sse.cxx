de17fed3b7a44a21697cd337c691d12ab7df1282
Rio Yokota <rioyokota@me.com>
2013-04-08 15:48:42 +0300

    Update simd.h

--- test/direct_sse.cxx
+++ test/direct_sse.cxx
@@ -20,15 +20,15 @@ void P2Psse(float4 *target, float4 *source, int ni, int nj, float eps2) {
     __m128 xi = _mm_setr_ps(source[i].x,source[i+1].x,source[i+2].x,source[i+3].x);   // xi = target->x
     __m128 yi = _mm_setr_ps(source[i].y,source[i+1].y,source[i+2].y,source[i+3].y);   // yi = target->y
     __m128 zi = _mm_setr_ps(source[i].z,source[i+1].z,source[i+2].z,source[i+3].z);   // zi = target->z
-    __m128 R2 = _mm_load1_ps(&eps2);      // R2 = eps2
+    __m128 R2 = _mm_set1_ps(eps2);      // R2 = eps2
   
-    __m128 x2 = _mm_load1_ps(&source[0].x); // x2 = source->x
+    __m128 x2 = _mm_set1_ps(source[0].x); // x2 = source->x
     x2 = _mm_sub_ps(x2, xi);              // x2 = x2 - xi
-    __m128 y2 = _mm_load1_ps(&source[0].y); // y2 = source->y
+    __m128 y2 = _mm_set1_ps(source[0].y); // y2 = source->y
     y2 = _mm_sub_ps(y2, yi);              // y2 = y2 - yi
-    __m128 z2 = _mm_load1_ps(&source[0].z); // z2 = source->z
+    __m128 z2 = _mm_set1_ps(source[0].z); // z2 = source->z
     z2 = _mm_sub_ps(z2, zi);              // z2 = z2 - zi
-    __m128 mj = _mm_load1_ps(&source[0].w); // mj = source->w
+    __m128 mj = _mm_set1_ps(source[0].w); // mj = source->w
   
     __m128 xj = x2;                       // xj = x2
     x2 = _mm_mul_ps(x2, x2);              // x2 = x2 * x2
@@ -41,62 +41,54 @@ void P2Psse(float4 *target, float4 *source, int ni, int nj, float eps2) {
     R2 = _mm_add_ps(R2, z2);              // R2 = R2 + z2
     __m128 invR;  
 
-    x2 = _mm_load_ps(&source[1].x);       // x2 = next source->x,y,z,w 
-    y2 = x2;                              // y2 = x2;
-    z2 = x2;                              // z2 = x2;
+    x2 = _mm_set1_ps(source[1].x);      // x2 = source->x 
+    y2 = _mm_set1_ps(source[1].y);      // y2 = source->y
+    z2 = _mm_set1_ps(source[1].z);      // z2 = source->z
     for( int j=0; j<nj-2; j++ ) {
       invR = _mm_rsqrt_ps(R2);            // invR = rsqrt(R2)       1
-      R2 = _mm_load1_ps(&eps2);           // R2 = eps2
-      x2 = _mm_shuffle_ps(x2, x2, 0x00);  // x2 = source->x
+      R2 = _mm_set1_ps(eps2);           // R2 = eps2
       x2 = _mm_sub_ps(x2, xi);            // x2 = x2 - xi           2
-      y2 = _mm_shuffle_ps(y2, y2, 0x55);  // y2 = source->y
       y2 = _mm_sub_ps(y2, yi);            // y2 = y2 - yi           3
-      z2 = _mm_shuffle_ps(z2, z2, 0xaa);  // z2 = source->z
       z2 = _mm_sub_ps(z2, zi);            // z2 = z2 - zi           4
   
       mj = _mm_mul_ps(mj, invR);          // mj = mj * invR         5
       phi = _mm_add_ps(phi, mj);          // phi = phi + mj         6
       invR = _mm_mul_ps(invR, invR);      // invR = invR * invR     7
       invR = _mm_mul_ps(invR, mj);        // invR = invR * mj       8
-      mj = _mm_load_ps(&source[j+1].x);   // mj = source->x,y,z,w
-      mj = _mm_shuffle_ps(mj, mj, 0xff);  // mj = source->w
+      mj = _mm_set1_ps(source[j+1].w);  // mj = source->w
   
       xj = _mm_mul_ps(xj, invR);          // xj = xj * invR         9
       ax = _mm_add_ps(ax, xj);            // ax = ax + xj          10
       xj = x2;                            // xj = x2
       x2 = _mm_mul_ps(x2, x2);            // x2 = x2 * x2          11
       R2 = _mm_add_ps(R2, x2);            // R2 = R2 + x2          12
-      x2 = _mm_load_ps(&source[j+2].x);   // x2 = next source->x,y,z,w
+      x2 = _mm_set1_ps(source[j+2].x);  // x2 = source->x
   
       yj = _mm_mul_ps(yj, invR);          // yj = yj * invR        13
       ay = _mm_add_ps(ay, yj);            // ay = ay + yj          14
       yj = y2;                            // yj = y2
       y2 = _mm_mul_ps(y2, y2);            // y2 = y2 * y2          15
       R2 = _mm_add_ps(R2, y2);            // R2 = R2 + y2          16
-      y2 = x2;                            // y2 = x2
+      y2 = _mm_set1_ps(source[j+2].y);  // y2 = source->y
   
       zj = _mm_mul_ps(zj, invR);          // zj = zj * invR        17
       az = _mm_add_ps(az, zj);            // az = az + zj          18
       zj = z2;                            // zj = z2
       z2 = _mm_mul_ps(z2, z2);            // z2 = z2 * z2          19
       R2 = _mm_add_ps(R2, z2);            // R2 = R2 + z2          20
-      z2 = x2;                            // z2 = x2
+      z2 = _mm_set1_ps(source[j+2].z);  // z2 = source->z
     }
     invR = _mm_rsqrt_ps(R2);              // invR = rsqrt(R2)
-    R2 = _mm_load1_ps(&eps2);             // R2 = eps2
-    x2 = _mm_shuffle_ps(x2, x2, 0x00);    // x2 = source->x
+    R2 = _mm_set1_ps(eps2);             // R2 = eps2
     x2 = _mm_sub_ps(x2, xi);              // x2 = x2 - xi
-    y2 = _mm_shuffle_ps(y2, y2, 0x55);    // y2 = source->y
     y2 = _mm_sub_ps(y2, yi);              // y2 = y2 - yi
-    z2 = _mm_shuffle_ps(z2, z2, 0xaa);    // z2 = source->z
     z2 = _mm_sub_ps(z2, zi);              // z2 = z2 - zi
   
     mj = _mm_mul_ps(mj, invR);            // mj = mj * invR
     phi = _mm_add_ps(phi, mj);            // phi = phi + mj
     invR = _mm_mul_ps(invR, invR);        // invR = invR * invR
     invR = _mm_mul_ps(invR, mj);          // invR = invR * mj
-    mj = _mm_load_ps(&source[nj-1].x);    // mj = source->x,y,z,w
-    mj = _mm_shuffle_ps(mj, mj, 0xff);    // mj = source->w
+    mj = _mm_set1_ps(source[nj-1].w);   // mj = source->w
   
     xj = _mm_mul_ps(xj, invR);            // xj = xj * invR
     ax = _mm_add_ps(ax, xj);              // ax = ax + xj
--- test/simd.h
+++ test/simd.h
@@ -2,77 +2,74 @@
 #define simd_h
 #include <iostream>
 #include <immintrin.h>
+
 class fvec8 {
 private:
-  __m256 ymm;
+  __m256 data;
 public:
   fvec8() {}                                                       // Default constructor
-  fvec8(const float a) {                                           // Copy constructor scalar
-    ymm = _mm256_set1_ps(a);
+  fvec8(const float v) {                                           // Copy constructor scalar
+    data = _mm256_set1_ps(v);
   }
-  fvec8(const __m256 a) {                                          // Copy constructor vector (__m256)
-    ymm = a;
+  fvec8(const __m256 v) {                                          // Copy constructor SIMD register
+    data = v;
   }
-  fvec8(const fvec8 &a) {                                          // Copy constructor vector (fvec8)
-    ymm = a.ymm;
+  fvec8(const fvec8 &v) {                                          // Copy constructor vector
+    data = v.data;
   }
   fvec8(const float a, const float b, const float c, const float d,
         const float e, const float f, const float g, const float h) {// Copy constructor (component-wise)
-    ymm = _mm256_setr_ps(a,b,c,d,e,f,g,h);
+    data = _mm256_setr_ps(a,b,c,d,e,f,g,h);
   }
   ~fvec8(){}                                                       // Destructor
-  const fvec8 &operator=(const float a) {                          // Scalar assignment
-    ymm = _mm256_set1_ps(a);
+  const fvec8 &operator=(const float v) {                          // Scalar assignment
+    data = _mm256_set1_ps(v);
     return *this;
   }
-  const fvec8 &operator=(const __m256 a) {                         // Vector assignment (__m256)
-    ymm = a;
+  const fvec8 &operator=(const fvec8 &v) {                         // Vector assignment
+    data = v.data;
     return *this;
   }
-  const fvec8 &operator=(const fvec8 &a) {                         // Vector assignment (fvec8)
-    ymm = a.ymm;
+  const fvec8 &operator+=(const fvec8 &v) {                        // Vector compound assignment (add)
+    data = _mm256_add_ps(data,v.data);
     return *this;
   }
-  const fvec8 &operator+=(const fvec8 &a) {                        // Vector compound assignment (add)
-    ymm = _mm256_add_ps(ymm,a.ymm);
+  const fvec8 &operator-=(const fvec8 &v) {                        // Vector compound assignment (subtract)
+    data = _mm256_sub_ps(data,v.data);
     return *this;
   }
-  const fvec8 &operator-=(const fvec8 &a) {                        // Vector compound assignment (subtract)
-    ymm = _mm256_sub_ps(ymm,a.ymm);
+  const fvec8 &operator*=(const fvec8 &v) {                        // Vector compound assignment (multiply)
+    data = _mm256_mul_ps(data,v.data);
     return *this;
   }
-  const fvec8 &operator*=(const fvec8 &a) {                        // Vector compound assignment (multiply)
-    ymm = _mm256_mul_ps(ymm,a.ymm);
+  const fvec8 &operator/=(const fvec8 &v) {                        // Vector compound assignment (divide)
+    data = _mm256_div_ps(data,v.data);
     return *this;
   }
-  const fvec8 &operator/=(const fvec8 &a) {                        // Vector compound assignment (divide)
-    ymm = _mm256_div_ps(ymm,a.ymm);
-    return *this;
+  fvec8 operator+(const fvec8 &v) const {                          // Vector arithmetic (add)
+    return fvec8(_mm256_add_ps(data,v.data));
   }
-  fvec8 operator+(const fvec8 &a) const {                          // Vector arithmetic (add)
-    return fvec8(_mm256_add_ps(ymm,a.ymm));
+  fvec8 operator-(const fvec8 &v) const {                          // Vector arithmetic (subtract)
+    return fvec8(_mm256_sub_ps(data,v.data));
   }
-  fvec8 operator-(const fvec8 &a) const {                          // Vector arithmetic (subtract)
-    return fvec8(_mm256_sub_ps(ymm,a.ymm));
+  fvec8 operator*(const fvec8 &v) const {                          // Vector arithmetic (multiply)
+    return fvec8(_mm256_mul_ps(data,v.data));
   }
-  fvec8 operator*(const fvec8 &a) const {                          // Vector arithmetic (multiply)
-    return fvec8(_mm256_mul_ps(ymm,a.ymm));
-  }
-  fvec8 operator/(const fvec8 &a) const {                          // Vector arithmetic (divide)
-    return fvec8(_mm256_div_ps(ymm,a.ymm));
+  fvec8 operator/(const fvec8 &v) const {                          // Vector arithmetic (divide)
+    return fvec8(_mm256_div_ps(data,v.data));
   }
   float &operator[](int i) {                                       // Indexing (lvalue)
-    return ((float*)&ymm)[i];
+    return ((float*)&data)[i];
   }
   const float &operator[](int i) const {                           // Indexing (rvalue)
-    return ((float*)&ymm)[i];
+    return ((float*)&data)[i];
   }
-  friend std::ostream &operator<<(std::ostream &s, const fvec8 &a) {// Component-wise output stream
-    for (int i=0; i<8; i++) s<<((float*)&a)[i]<<' ';
+  friend std::ostream &operator<<(std::ostream &s, const fvec8 &v) {// Component-wise output stream
+    for (int i=0; i<8; i++) s << v[i] << ' ';
     return s;
   }
-  friend fvec8 rsqrt(const fvec8 &a) {
-    return fvec8(_mm256_rsqrt_ps(a.ymm));
+  friend fvec8 rsqrt(const fvec8 &v) {                             // reciprocal square root
+    return fvec8(_mm256_rsqrt_ps(v.data));
   }
 };
 
