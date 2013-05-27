f6c41f2d5e00e852425cb3fc3a63edf00988c416
Rio Yokota <rioyokota@me.com>
2013-05-27 23:48:01 +0900

    Updated .gitignore.

--- test/cpufmm2d.cxx
+++ test/cpufmm2d.cxx
@@ -39,10 +39,8 @@ __m128 log2f4(__m128 x) {
    return _mm_add_ps(p, e);
 }
 
-const int N = 128*200;
 const int NCRIT = 100;
-const int MAXLEVEL = N >= NCRIT ? 1 + int(log(N / NCRIT)/M_LN2/2) : 0;
-const float THETA = 0.75;
+const float THETA = 0.5;
 const float EPS2 = 0.00001;
 const float X0 = .5;
 const float R0 = .5;
@@ -56,8 +54,7 @@ double get_time() {
 struct Body {
   int ICELL;
   float X[2];
-  float M;
-  float P;
+  float S;
 };
 
 struct Cell {
@@ -72,7 +69,8 @@ struct Cell {
   float M[4];
 };
 
-void getIndex(Body *bodies) {
+void getIndex(int N, Body *bodies) {
+  int MAXLEVEL = N >= NCRIT ? 1 + int(log(N / NCRIT)/M_LN2/2) : 0;
   float diameter = 2 * R0 / (1 << MAXLEVEL);
   for( int i=0; i<N; i++ ) {
     int ix = (bodies[i].X[0] + R0 - X0) / diameter;
@@ -88,7 +86,7 @@ void getIndex(Body *bodies) {
   }
 }
 
-void sortBody(Body *bodies, Body *buffer) {
+void sortBody(int N, Body *bodies, Body *buffer) {
   int Imin = bodies[0].ICELL;
   int Imax = bodies[0].ICELL;
   for( int i=0; i<N; ++i ) {
@@ -123,7 +121,8 @@ void initCell(const Body *bodies, Cell &cell, const int &icell, const int &leaf,
   for( int i=0; i<4; i++ ) cell.M[i] = 0;
 }
 
-void buildCell(const Body *bodies, Cell *cells, int &ncell) {
+void buildCell(int N, const Body *bodies, Cell *cells, int &ncell) {
+  int MAXLEVEL = N >= NCRIT ? 1 + int(log(N / NCRIT)/M_LN2/2) : 0;
   int oldcell = -1;
   ncell = 0;
   float diameter = 2 * R0 / (1 << MAXLEVEL);
@@ -139,7 +138,8 @@ void buildCell(const Body *bodies, Cell *cells, int &ncell) {
   }
 }
 
-void buildTree(const Body *bodies, Cell *cells, int &ncell) {
+void buildTree(int N, const Body *bodies, Cell *cells, int &ncell) {
+  int MAXLEVEL = N >= NCRIT ? 1 + int(log(N / NCRIT)/M_LN2/2) : 0;
   int begin = 0, end = ncell;
   float diameter = 2 * R0 / (1 << MAXLEVEL);
   for( int level=MAXLEVEL-1; level>=0; level-- ) {
@@ -172,15 +172,15 @@ void setCenter(Body *bodies, Cell *cells, Cell &cell) {
   float X[2] = {0};
   for( int i=0; i<cell.NCLEAF; i++ ) {
     Body body = bodies[cell.LEAF+i];
-    M += body.M;
-    X[0] += body.X[0] * body.M;
-    X[1] += body.X[1] * body.M;
+    M += body.S;
+    X[0] += body.X[0] * body.S;
+    X[1] += body.X[1] * body.S;
   }
   for( int c=0; c<cell.NCHILD; c++ ) {
     Cell child = cells[cell.CHILD+c];
-    M += fabs(child.M[0]);
-    X[0] += child.X[0] * fabs(child.M[0]);
-    X[1] += child.X[1] * fabs(child.M[0]);
+    M += child.M[0];
+    X[0] += child.X[0] * child.M[0];
+    X[1] += child.X[1] * child.M[0];
   }
   X[0] /= M;
   X[1] /= M;
@@ -194,10 +194,10 @@ inline void P2M(Cell &cell, const Body *bodies) {
     Body leaf = bodies[cell.LEAF+i];
     float dx = cell.X[0] - leaf.X[0];
     float dy = cell.X[1] - leaf.X[1];
-    cell.M[0] += leaf.M;
-    cell.M[1] += leaf.M * dx * dx / 2;
-    cell.M[2] += leaf.M * dx * dy / 2;
-    cell.M[3] += leaf.M * dy * dy / 2;
+    cell.M[0] += leaf.S;
+    cell.M[1] += leaf.S * dx * dx / 2;
+    cell.M[2] += leaf.S * dx * dy / 2;
+    cell.M[3] += leaf.S * dy * dy / 2;
   }
 }
 
@@ -213,22 +213,22 @@ inline void M2M(Cell &parent, const Cell *cells) {
   }
 }
 
-inline void P2P(Body *bodies, const Cell &icell, const Cell &jcell) {
-  Body *ileaf = &bodies[icell.LEAF];
-  Body *jleaf = &bodies[jcell.LEAF];
+inline void P2P(Body *ibodies, Body *jbodies, const Cell &icell, const Cell &jcell) {
+  Body *ileaf = &ibodies[icell.LEAF];
+  Body *jleaf = &jbodies[jcell.LEAF];
   for( int i=0; i<icell.NDLEAF; i++ ) {
     for( int j=0; j<jcell.NDLEAF; j++ ) {
       float dx = ileaf[i].X[0] - jleaf[j].X[0];
       float dy = ileaf[i].X[1] - jleaf[j].X[1];
       float R = sqrtf(dx * dx + dy * dy + EPS2);
-      ileaf[i].P += jleaf[j].M * logf(R);
+      ileaf[i].S += jleaf[j].S * logf(R);
     }
   }
 }
 
-inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
-  Body *ileaf = &bodies[icell.LEAF];
-  Body *jleaf = &bodies[jcell.LEAF];
+inline void P2PSSE(Body *ibodies, Body *jbodies, const Cell &icell, const Cell &jcell) {
+  Body *ileaf = &ibodies[icell.LEAF];
+  Body *jleaf = &jbodies[jcell.LEAF];
 
   __m128 BASE = _mm_set1_ps(M_E);
   BASE = log2f4(BASE);
@@ -243,7 +243,7 @@ inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
     X2 = _mm_sub_ps(XI, X2);
     __m128 Y2 = _mm_set1_ps(jleaf[0].X[1]);
     Y2 = _mm_sub_ps(YI, Y2);
-    __m128 MJ = _mm_set1_ps(jleaf[0].M);
+    __m128 MJ = _mm_set1_ps(jleaf[0].S);
 
     __m128 XJ = X2;
     X2 = _mm_mul_ps(X2,X2);
@@ -265,7 +265,7 @@ inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
       logR = _mm_mul_ps(logR,BASE);
       MJ = _mm_mul_ps(MJ,logR);
       PHI = _mm_add_ps(PHI,MJ);
-      MJ = _mm_set1_ps(jleaf[j+1].M);
+      MJ = _mm_set1_ps(jleaf[j+1].S);
 
       XJ = X2;
       X2 = _mm_mul_ps(X2,X2);
@@ -286,7 +286,7 @@ inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
     logR = _mm_mul_ps(logR,BASE);
     MJ = _mm_mul_ps(MJ,logR);
     PHI = _mm_add_ps(PHI,MJ);
-    MJ = _mm_set1_ps(jleaf[jcell.NDLEAF-1].M);
+    MJ = _mm_set1_ps(jleaf[jcell.NDLEAF-1].S);
 
     XJ = X2;
     X2 = _mm_mul_ps(X2,X2);
@@ -302,7 +302,7 @@ inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
     MJ = _mm_mul_ps(MJ,logR);
     PHI = _mm_add_ps(PHI,MJ);
     for (int k=0; k<4; k++) {
-      ileaf[i+k].P -= ((float*)&PHI)[k];
+      ileaf[i+k].S -= ((float*)&PHI)[k];
     }
   }
   for( int i=(icell.NDLEAF-1)/4*4; i<icell.NDLEAF; i++ ) {
@@ -310,31 +310,29 @@ inline void P2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
       float dx = ileaf[i].X[0] - jleaf[j].X[0];
       float dy = ileaf[i].X[1] - jleaf[j].X[1];
       float R = sqrtf(dx * dx + dy * dy + EPS2);
-      ileaf[i].P += jleaf[j].M * logf(R);
+      ileaf[i].S += jleaf[j].S * logf(R);
     }
   }
 }
 
-inline void M2P(Body *bodies, const Cell &icell, const Cell &jcell) {
+inline void M2P(Body *ibodies, const Cell &icell, const Cell &jcell) {
   for( int i=0; i<icell.NDLEAF; i++ ) {
-    Body *ileaf = &bodies[icell.LEAF];
+    Body *ileaf = &ibodies[icell.LEAF];
     float dx = ileaf[i].X[0] - jcell.X[0];
     float dy = ileaf[i].X[1] - jcell.X[1];
     float R = sqrtf(dx * dx + dy * dy + EPS2);
     float invR = 1 / R;
     float invR2 = invR * invR;
     float invR4 = -2 * invR2 * invR2;
-    ileaf[i].P += jcell.M[0] * log(R);
-    ileaf[i].P += jcell.M[1] * (dx * dx * invR4 + invR2);
-    ileaf[i].P += jcell.M[2] * dx * dy * invR4;
-    ileaf[i].P += jcell.M[3] * (dy * dy * invR4 + invR2);
+    ileaf[i].S += jcell.M[0] * log(R);
+    ileaf[i].S += jcell.M[1] * (dx * dx * invR4 + invR2);
+    ileaf[i].S += jcell.M[2] * dx * dy * invR4;
+    ileaf[i].S += jcell.M[3] * (dy * dy * invR4 + invR2);
   }
 }
 
-inline void M2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
-  Body *ileaf = &bodies[icell.LEAF];
-  Body *jleaf = &bodies[jcell.LEAF];
-
+inline void M2PSSE(Body *ibodies, const Cell &icell, const Cell &jcell) {
+  Body *ileaf = &ibodies[icell.LEAF];
   __m128 BASE = _mm_set1_ps(M_E);
   BASE = log2f4(BASE);
   BASE = _mm_div_ps(_mm_set1_ps(1.0f),BASE);
@@ -379,7 +377,7 @@ inline void M2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
     Y2 = _mm_mul_ps(Y2,M3);
     PHI = _mm_add_ps(PHI,Y2);
     for (int k=0; k<4; k++) {
-      ileaf[i+k].P += ((float*)&PHI)[k];
+      ileaf[i+k].S += ((float*)&PHI)[k];
     }
   }
   for( int i=(icell.NDLEAF-1)/4*4; i<icell.NDLEAF; i++ ) {
@@ -389,55 +387,94 @@ inline void M2PSSE(Body *bodies, const Cell &icell, const Cell &jcell) {
     float invR = 1 / R;
     float invR2 = invR * invR;
     float invR4 = -2 * invR2 * invR2;
-    ileaf[i].P += jcell.M[0] * logf(R);
-    ileaf[i].P += jcell.M[1] * (dx * dx * invR4 + invR2);
-    ileaf[i].P += jcell.M[2] * dx * dy * invR4;
-    ileaf[i].P += jcell.M[3] * (dy * dy * invR4 + invR2);
+    ileaf[i].S += jcell.M[0] * logf(R);
+    ileaf[i].S += jcell.M[1] * (dx * dx * invR4 + invR2);
+    ileaf[i].S += jcell.M[2] * dx * dy * invR4;
+    ileaf[i].S += jcell.M[3] * (dy * dy * invR4 + invR2);
   }
 }
 
 int main() {
-  Body *bodies = new Body [N];
-  Body *bodies2 = new Body [N];
+  const int ni = 10000;
+  const int nj = 40000;
+  Body *ibodies = new Body [ni];
+  Body *ibodies2 = new Body [ni];
+  Body *jbodies = new Body [nj];
+  Body *jbodies2 = new Body [nj];
 // Initialize
-  std::cout << "N     : " << N << std::endl << std::endl;
-  for( int i=0; i!=N; ++i ) {
-    bodies[i].X[0] = drand48();
-    bodies[i].X[1] = drand48();
-    bodies[i].M = 1. / N;
-    bodies[i].P = -bodies[i].M / sqrtf(EPS2);
+  std::cout << "ni    : " << ni << std::endl;
+  std::cout << "nj    : " << nj << std::endl << std::endl;
+  for( int i=0; i!=ni; ++i ) {
+    ibodies[i].X[0] = drand48();
+    ibodies[i].X[1] = drand48();
+    ibodies[i].S = 0;
+  }
+  int nj1 = 0, nj2 = 0;
+  for( int i=0; i!=nj; ++i ) {
+    float c = (drand48() - .5) / nj;
+    if (c > 0) {
+      jbodies[nj1].X[0] = drand48();
+      jbodies[nj1].X[1] = drand48();
+      jbodies[nj1].S = c;
+      nj1++;
+    } else {
+      jbodies2[nj2].X[0] = drand48();
+      jbodies2[nj2].X[1] = drand48();
+      jbodies2[nj2].S = c;
+      nj2++;
+    }
   }
 // Set root cell
   double tic = get_time();
 // Build tree
-  getIndex(bodies);
-  Body *buffer = new Body [N];
-  sortBody(bodies,buffer);
+  getIndex(ni,ibodies);
+  getIndex(nj1,jbodies);
+  getIndex(nj2,jbodies2);
+  Body *buffer = new Body [ni];
+  sortBody(ni,ibodies,buffer);
   delete[] buffer; 
-  Cell *cells = new Cell [N];
-  int ncell = 0;
-  buildCell(bodies,cells,ncell);
+  buffer = new Body [nj];
+  sortBody(nj1,jbodies,buffer);
+  sortBody(nj2,jbodies2,buffer);
+  delete[] buffer; 
+  Cell *icells = new Cell [ni];
+  Cell *jcells = new Cell [nj1];
+  Cell *jcells2 = new Cell [nj2];
+  int nicell = 0, njcell = 0, njcell2 = 0;
+  buildCell(ni,ibodies,icells,nicell);
+  buildCell(nj1,jbodies,jcells,njcell);
+  buildCell(nj2,jbodies2,jcells2,njcell2);
   double toc = get_time();
   std::cout << "Index : " << toc-tic << std::endl;
   tic = get_time();
-  int ntwig = ncell;
-  buildTree(bodies,cells,ncell);
+  int ntwig = nicell;
+  buildTree(ni,ibodies,icells,nicell);
+  buildTree(nj1,jbodies,jcells,njcell);
+  buildTree(nj2,jbodies2,jcells2,njcell2);
 // Upward sweep
-  for( int i=0; i<ncell-1; i++ ) {
-    setCenter(bodies,cells,cells[i]);
-    P2M(cells[i],bodies);
-    M2M(cells[i],cells);
+  for( int i=0; i<njcell-1; i++ ) {
+    setCenter(jbodies,jcells,jcells[i]);
+    P2M(jcells[i],jbodies);
+    M2M(jcells[i],jcells);
+  }
+  for( int i=0; i<njcell2-1; i++ ) {
+    setCenter(jbodies2,jcells2,jcells2[i]);
+    P2M(jcells2[i],jbodies2);
+    M2M(jcells2[i],jcells2);
   }
   toc = get_time();
   std::cout << "Build : " << toc-tic << std::endl;
 // Direct summation
   tic = get_time();
 #if 1
-  Cell root = cells[ncell-1];
-  P2PSSE(bodies,root,root);
-  for( int i=0; i<N; i++ ) {
-    bodies2[i].P = bodies[i].P;
-    bodies[i].P = -bodies[i].M / sqrtf(EPS2);
+  Cell iroot = icells[nicell-1];
+  Cell jroot = jcells[njcell-1];
+  Cell jroot2 = jcells2[njcell2-1];
+  P2PSSE(ibodies,jbodies,iroot,jroot);
+  P2PSSE(ibodies,jbodies2,iroot,jroot2);
+  for( int i=0; i<ni; i++ ) {
+    ibodies2[i].S = ibodies[i].S;
+    ibodies[i].S = 0;
   }
 #endif
   toc = get_time();
@@ -448,20 +485,39 @@ int main() {
   int stack[20];
   int nstack = 0;
   for( int i=0; i<ntwig; i++ ) {
-    Cell icell = cells[i];
-    stack[nstack++] = ncell-1;
+    Cell icell = icells[i];
+    stack[nstack++] = njcell-1;
     while( nstack ) {
-      Cell jparent = cells[stack[--nstack]];
+      Cell jparent = jcells[stack[--nstack]];
       for( int j=0; j<jparent.NCHILD; j++ ) {
-        Cell jcell = cells[jparent.CHILD+j];
+        Cell jcell = jcells[jparent.CHILD+j];
         float dx = icell.X[0] - jcell.X[0];
         float dy = icell.X[1] - jcell.X[1];
         float R = sqrtf(dx * dx + dy * dy);
         if( jcell.R < THETA * R ) {
-          M2PSSE(bodies,icell,jcell);
+          M2PSSE(ibodies,icell,jcell);
           NM2P++;
         } else if( jcell.NCHILD == 0 ) {
-          P2PSSE(bodies,icell,jcell);
+          P2PSSE(ibodies,jbodies,icell,jcell);
+          NP2P++;
+        } else {
+          stack[nstack++] = jparent.CHILD+j;
+        }
+      }
+    }
+    stack[nstack++] = njcell2-1;
+    while( nstack ) {
+      Cell jparent = jcells2[stack[--nstack]];
+      for( int j=0; j<jparent.NCHILD; j++ ) {
+        Cell jcell = jcells2[jparent.CHILD+j];
+        float dx = icell.X[0] - jcell.X[0];
+        float dy = icell.X[1] - jcell.X[1];
+        float R = sqrtf(dx * dx + dy * dy);
+        if( jcell.R < THETA * R ) {
+          M2PSSE(ibodies,icell,jcell);
+          NM2P++;
+        } else if( jcell.NCHILD == 0 ) {
+          P2PSSE(ibodies,jbodies2,icell,jcell);
           NP2P++;
         } else {
           stack[nstack++] = jparent.CHILD+j;
@@ -475,14 +531,18 @@ int main() {
   std::cout << "NM2P  : " << NM2P << std::endl << std::endl;
 // Check accuracy
   float err = 0, rel = 0;
-  for( int i=0; i<N; i++ ) {
-    Body body = bodies[i];
-    Body body2 = bodies2[i];
-    err += (body2.P - body.P) * (body2.P - body.P);
-    rel += body2.P * body2.P;
+  for( int i=0; i<ni; i++ ) {
+    Body body = ibodies[i];
+    Body body2 = ibodies2[i];
+    err += (body2.S - body.S) * (body2.S - body.S);
+    rel += body2.S * body2.S;
   }
   std::cout << "P err : " << sqrtf(err/rel) << std::endl;
-  delete[] bodies;
-  delete[] bodies2;
-  delete[] cells;
+  delete[] ibodies;
+  delete[] ibodies2;
+  delete[] jbodies;
+  delete[] jbodies2;
+  delete[] icells;
+  delete[] jcells;
+  delete[] jcells2;
 }
