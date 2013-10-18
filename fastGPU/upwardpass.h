ff282e5d7f8c737885a2a3b12277565a24f05796
Rio Yokota <rioyokota@me.com>
2013-10-18 12:13:31 +0300

    normalize2

--- fastGPU/upwardpass.h
+++ fastGPU/upwardpass.h
@@ -67,7 +67,7 @@ namespace {
     const int begin = cell.body();
     const int size = cell.nbody();
     const int end = begin + size;
-    const float3 com = setCenter(size,bodyPos+begin,1);
+    const float3 center = setCenter(size,bodyPos+begin,1);
     float4 M[3];
     const float huge = 1e10f;
     float3 Xmin = {+huge, +huge, +huge};
@@ -83,9 +83,9 @@ namespace {
       M[2].y += body.w * body.y * body.z;
       pairMinMax(Xmin, Xmax, body, body);
     }
-    M[0].x = com.x;
-    M[0].y = com.y;
-    M[0].z = com.z;
+    M[0].x = center.x;
+    M[0].y = center.y;
+    M[0].z = center.z;
     M[1].x -= M[0].x * M[0].x * M[0].w;
     M[1].y -= M[0].y * M[0].y * M[0].w;
     M[1].z -= M[0].z * M[0].z * M[0].w;
@@ -94,14 +94,14 @@ namespace {
     M[2].y -= M[0].y * M[0].z * M[0].w;
     const float3 X = {(Xmax.x+Xmin.x)*0.5f, (Xmax.y+Xmin.y)*0.5f, (Xmax.z+Xmin.z)*0.5f};
     const float3 R = {(Xmax.x-Xmin.x)*0.5f, (Xmax.y-Xmin.y)*0.5f, (Xmax.z-Xmin.z)*0.5f};
-    const float dx = X.x - com.x;
-    const float dy = X.y - com.y;
-    const float dz = X.z - com.z;
+    const float dx = X.x - center.x;
+    const float dy = X.y - center.y;
+    const float dz = X.z - center.z;
     const float  s = sqrt(dx*dx + dy*dy + dz*dz);
     const float  l = max(2.0f*max(R.x, max(R.y, R.z)), 1.0e-6f);
     const float cellOp = l*invTheta + s;
     const float cellOp2 = cellOp*cellOp;
-    sourceCenter[cellIdx] = make_float4(com.x, com.y, com.z, cellOp2);
+    sourceCenter[cellIdx] = make_float4(center.x, center.y, center.z, cellOp2);
     for (int i=0; i<3; i++) Multipole[3*cellIdx+i] = M[i];
     cellXmin[cellIdx] = make_float4(Xmin.x, Xmin.y, Xmin.z, 0.0f);
     cellXmax[cellIdx] = make_float4(Xmax.x, Xmax.y, Xmax.z, 0.0f);
@@ -123,7 +123,7 @@ namespace {
     const int size = cell.nchild();
     const int end = begin + size;
     if (cell.isLeaf()) return;
-    const float3 com = setCenter(size,Multipole+3*begin,3);
+    const float3 center = setCenter(size,Multipole+3*begin,3);
     float4 Mi[3], Mj[3];
     const float huge = 1e10f;
     float3 Xmin = {+huge, +huge, +huge};
@@ -131,17 +131,17 @@ namespace {
     for( int i=begin; i<end; i++ ) {
       Mj[0] = Multipole[3*i];
       Mi[0].w += Mj[0].w;
-      Mi[1].x += Mj[0].w * (com.x * com.x + Mj[1].x);
-      Mi[1].y += Mj[0].w * (com.y * com.y + Mj[1].y);
-      Mi[1].z += Mj[0].w * (com.z * com.z + Mj[1].z);
-      Mi[1].w += Mj[0].w * (com.x * com.y + Mj[1].w);
-      Mi[2].x += Mj[0].w * (com.x * com.z + Mj[2].x);
-      Mi[2].y += Mj[0].w * (com.y * com.z + Mj[2].y);
+      Mi[1].x += Mj[0].w * (center.x * center.x + Mj[1].x);
+      Mi[1].y += Mj[0].w * (center.y * center.y + Mj[1].y);
+      Mi[1].z += Mj[0].w * (center.z * center.z + Mj[1].z);
+      Mi[1].w += Mj[0].w * (center.x * center.y + Mj[1].w);
+      Mi[2].x += Mj[0].w * (center.x * center.z + Mj[2].x);
+      Mi[2].y += Mj[0].w * (center.y * center.z + Mj[2].y);
       pairMinMax(Xmin, Xmax, cellXmin[i], cellXmax[i]);
     }
-    Mi[0].x = com.x;
-    Mi[0].y = com.y;
-    Mi[0].z = com.z;
+    Mi[0].x = center.x;
+    Mi[0].y = center.y;
+    Mi[0].z = center.z;
     Mi[1].x -= Mi[0].x * Mi[0].x * Mi[0].w;
     Mi[1].y -= Mi[0].y * Mi[0].y * Mi[0].w;
     Mi[1].z -= Mi[0].z * Mi[0].z * Mi[0].w;
@@ -150,14 +150,14 @@ namespace {
     Mi[2].y -= Mi[0].y * Mi[0].z * Mi[0].w;
     const float3 X = {(Xmax.x+Xmin.x)*0.5f, (Xmax.y+Xmin.y)*0.5f, (Xmax.z+Xmin.z)*0.5f};
     const float3 R = {(Xmax.x-Xmin.x)*0.5f, (Xmax.y-Xmin.y)*0.5f, (Xmax.z-Xmin.z)*0.5f};
-    const float dx = X.x - com.x;
-    const float dy = X.y - com.y;
-    const float dz = X.z - com.z;
+    const float dx = X.x - center.x;
+    const float dy = X.y - center.y;
+    const float dz = X.z - center.z;
     const float  s = sqrt(dx*dx + dy*dy + dz*dz);
     const float  l = max(2.0f*max(R.x, max(R.y, R.z)), 1.0e-6f);
     const float cellOp = l*invTheta + s;
     const float cellOp2 = cellOp*cellOp;
-    sourceCenter[cellIdx] = make_float4(com.x, com.y, com.z, cellOp2);
+    sourceCenter[cellIdx] = make_float4(center.x, center.y, center.z, cellOp2);
     for (int i=0; i<3; i++) Multipole[3*cellIdx+i] = Mi[i];
     cellXmin[cellIdx] = make_float4(Xmin.x, Xmin.y, Xmin.z, 0.0f);
     cellXmax[cellIdx] = make_float4(Xmax.x, Xmax.y, Xmax.z, 0.0f);
