069b5691b4c682eeee50908d60b4b300a5253b89
Rio Yokota <rioyokota@me.com>
2013-11-02 18:29:59 +0900

    Minor changes.

--- fastGPU/types.h
+++ fastGPU/types.h
@@ -15,7 +15,7 @@
 
 const int P = 3;
 const int NTERM = P*(P+1)*(P+2)/6;
-const int NVEC4 = (NTERM - 1) / 4 + 1;
+const int NVEC4 = (NTERM-1)/4+1;
 typedef vec<3,float> fvec3;
 typedef vec<4,float> fvec4;
 typedef vec<NTERM,float> fvecP;
