d496cb941704e35d176d4875a7e7314d79aa408d
Rio Yokota <rioyokota@me.com>
2013-11-17 13:35:46 +0900

    Update radixbody.cxx

--- test/radixbody.cxx
+++ test/radixbody.cxx
@@ -4,8 +4,6 @@
 #include <omp.h>
 #include <sys/time.h>
 
-#define OMP_NUM_THREADS 12
-
 double get_time() {
   struct timeval tv;
   gettimeofday(&tv,NULL);
@@ -15,63 +13,78 @@ double get_time() {
 struct Body {
   int  IBODY;
   int  IPROC;
-  int  ICELL;
+  int  ISORT;
   float X[3];
   float SRC;
   float TRG[4];
 };
 
-void radixsort(int *a, int *b, int *p, int *q, int n) {
+void radixsort(int *key, int *value, int size) {
   const int bitStride = 8;
   const int stride = 1 << bitStride;
   const int mask = stride - 1;
-  int (*bucketPerThread)[stride] = new int [OMP_NUM_THREADS][stride]();
-  int aMaxPerThread[OMP_NUM_THREADS] = {0};
-  int aMax = 0;
+  int numThreads;
+  int maxKey = 0;
+  int (*bucketPerThread)[stride];
+  int * maxKeyPerThread;
+  int * buffer = new int [size];
+  int * permutation = new int [size];
 #pragma omp parallel
   {
-#pragma omp for
-    for( int i=0; i<n; i++ )
-      if( a[i] > aMaxPerThread[omp_get_thread_num()] )
-        aMaxPerThread[omp_get_thread_num()] = a[i];
+    numThreads = omp_get_num_threads();
 #pragma omp single
-    for( int i=0; i<OMP_NUM_THREADS; i++ )
-      if( aMaxPerThread[i] > aMax ) aMax = aMaxPerThread[i];
-    while( aMax > 0 ) {
+    {
+      bucketPerThread = new int [numThreads][stride]();
+      maxKeyPerThread = new int [numThreads];
+      for (int i=0; i<numThreads; i++)
+	maxKeyPerThread[i] = 0;
+    }
+#pragma omp for
+    for( int i=0; i<size; i++ )
+      if( key[i] > maxKeyPerThread[omp_get_thread_num()] )
+        maxKeyPerThread[omp_get_thread_num()] = key[i];
+#pragma omp single
+    for( int i=0; i<numThreads; i++ )
+      if( maxKeyPerThread[i] > maxKey ) maxKey = maxKeyPerThread[i];
+    while( maxKey > 0 ) {
       int bucket[stride] = {0};
 #pragma omp single
-      for( int t=0; t<OMP_NUM_THREADS; t++ )
+      for( int t=0; t<numThreads; t++ )
         for( int i=0; i<stride; i++ )
           bucketPerThread[t][i] = 0;
 #pragma omp for
-      for( int i=0; i<n; i++ )
-        bucketPerThread[omp_get_thread_num()][a[i] & mask]++;
+      for( int i=0; i<size; i++ )
+        bucketPerThread[omp_get_thread_num()][key[i] & mask]++;
 #pragma omp single
       {
-      for( int t=0; t<OMP_NUM_THREADS; t++ )
-        for( int i=0; i<stride; i++ )
-          bucket[i] += bucketPerThread[t][i];
-      for( int i=1; i<stride; i++ )
-        bucket[i] += bucket[i-1];
-      for( int i=n-1; i>=0; i-- )
-        q[i] = --bucket[a[i] & mask];
+	for( int t=0; t<numThreads; t++ )
+	  for( int i=0; i<stride; i++ )
+	    bucket[i] += bucketPerThread[t][i];
+	for( int i=1; i<stride; i++ )
+	  bucket[i] += bucket[i-1];
+	for( int i=size-1; i>=0; i-- )
+	  permutation[i] = --bucket[key[i] & mask];
       }
 #pragma omp for
-      for( int i=0; i<n; i++ )
-        b[q[i]] = p[i];
+      for( int i=0; i<size; i++ )
+        buffer[permutation[i]] = value[i];
 #pragma omp for
-      for( int i=0; i<n; i++ )
-        p[i] = b[i];
+      for( int i=0; i<size; i++ )
+        value[i] = buffer[i];
 #pragma omp for
-      for( int i=0; i<n; i++ )
-        b[q[i]] = a[i];
+      for( int i=0; i<size; i++ )
+        buffer[permutation[i]] = key[i];
 #pragma omp for
-      for( int i=0; i<n; i++ )
-        a[i] = b[i] >> bitStride;
+      for( int i=0; i<size; i++ )
+        key[i] = buffer[i] >> bitStride;
 #pragma omp single
-      aMax >>= bitStride;
+      maxKey >>= bitStride;
     }
   }
+  delete[] bucketPerThread;
+  delete[] maxKeyPerThread;
+  delete[] buffer;
+  delete[] permutation;
 }
  
  
@@ -80,34 +93,30 @@ int main()
   const int N = 10000000;
   Body *bodies = new Body [N];
   Body *buffer = new Body [N];
-  int *a = new int [N];
-  int *b = new int [N];
-  int *p = new int [N];
-  int *q = new int [N];
+  int *key = new int [N];
+  int *index = new int [N];
   for( int i=0; i<N; i++ ) {
-    bodies[i].ICELL = N / 10. * drand48();
+    bodies[i].ISORT = N / 10. * drand48();
     buffer[i] = bodies[i];
-    a[i] = bodies[i].ICELL;
-    p[i] = i;
+    key[i] = bodies[i].ISORT;
+    index[i] = i;
   }
   printf("N       : %d\n",N);
   double tic = get_time();
-  radixsort(a,b,p,q,N);
+  radixsort(key,index,N);
   double toc = get_time();
   printf("Sort    : %lf s\n",toc-tic);
   tic = get_time();
 #pragma omp parallel for
   for( int i=0; i<N; i++ ) {
-    bodies[i] = buffer[p[i]];
+    bodies[i] = buffer[index[i]];
   }
   toc = get_time();
   printf("Permute : %lf s\n",toc-tic);
   for( int i=1; i<N; i++ )
-    assert( bodies[i].ICELL >= bodies[i-1].ICELL );
+    assert( bodies[i].ISORT >= bodies[i-1].ISORT );
   delete[] bodies;
   delete[] buffer;
-  delete[] a;
-  delete[] b;
-  delete[] p;
-  delete[] q;
+  delete[] key;
+  delete[] index;
 }
