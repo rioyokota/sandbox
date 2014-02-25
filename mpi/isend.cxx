f67b33919266eeae513d60514a173887a74fa15c
Rio Yokota <rioyokota@me.com>
2014-02-25 11:26:03 +0300

    Update isend.cxx

--- mpi/isend.cxx
+++ mpi/isend.cxx
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <cmath>
 #include <iostream>
 #include <sys/time.h>
 
@@ -9,26 +10,46 @@ double get_time() {
 }
 
 int main(int argc, char **argv) {
-  const int N = 1000000;
-  int size,rank;
+  const int N = 10000000;
+  int size, rank;
+  double tic, toc;
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   float * send = new float [N];
   float * recv = new float [N];
+  for (int i=0; i<N; i++) send[i] = rank;
   int sendRank = (rank + 1) % size;
   int recvRank = (rank - 1 + size) % size;
   MPI_Request sendReq, recvReq;
-  double tic = get_time();
-  MPI_Isend(send, N, MPI_FLOAT, recvRank, 0, MPI_COMM_WORLD, &sendReq);
-  MPI_Irecv(recv, N, MPI_FLOAT, sendRank, 0, MPI_COMM_WORLD, &recvReq);
-  double toc = get_time();
-  if (rank == 0) std::cout << "Send : " << toc-tic << std::endl;
-  tic = get_time();
+  double TIC = get_time();
+#pragma omp parallel sections private(tic, toc)
+  {
+#pragma omp section
+    {
+      tic = get_time();
+      MPI_Isend(send, N, MPI_FLOAT, recvRank, 0, MPI_COMM_WORLD, &sendReq);
+      MPI_Irecv(recv, N, MPI_FLOAT, sendRank, 0, MPI_COMM_WORLD, &recvReq);
+      toc = get_time();
+      if (rank == 0) std::cout << "Send : " << toc-tic << std::endl;
+    }
+#pragma omp section
+    {
+      tic = get_time();
+      for (int i=0; i<N; i++) {
+	recv[i] = i * M_PI;
+      }
+      toc = get_time();
+      if (rank == 0) std::cout << "Calc : " << toc-tic << std::endl; 
+    }
+  }
   MPI_Wait(&sendReq, MPI_STATUS_IGNORE);
   MPI_Wait(&recvReq, MPI_STATUS_IGNORE);
-  toc = get_time();
-  if (rank == 0) std::cout << "Recv : " << toc-tic << std::endl;
+  double TOC = get_time();
+  if (rank == 0) std::cout << "Total: " << TOC-TIC << std::endl;
+  float sum = 0;
+  for (int i=0; i<N; i++) sum += recv[i];
+  //std::cout << "Rank : " << rank << " Sum : " << sum << std::endl;
   delete[] send;
   delete[] recv;
   MPI_Finalize();
