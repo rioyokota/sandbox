6048f071ba9843542754d755175bb85523b79a05
Rio Yokota <rioyokota@me.com>
2014-04-16 18:39:10 +0300

    Update binary.cxx

--- sort/binary.cxx
+++ sort/binary.cxx
@@ -7,15 +7,11 @@
 #include <vector>
 #include "types.h"
 
-typedef std::vector<int> Index;
-struct Position {
-  vec3 X;
-};
-typedef std::vector<Position> Positions;
-typedef std::vector<Position>::iterator P_iter;
-bool compareX(Position Pi, Position Pj) { return (Pi.X[0] < Pj.X[0]); }
-bool compareY(Position Pi, Position Pj) { return (Pi.X[1] < Pj.X[1]); }
-bool compareZ(Position Pi, Position Pj) { return (Pi.X[2] < Pj.X[2]); }
+typedef std::vector<vec3> Xvector;
+typedef std::vector<vec3>::iterator X_iter;
+bool compareX(vec3 Xi, vec3 Xj) { return (Xi[0] < Xj[0]); }
+bool compareY(vec3 Xi, vec3 Xj) { return (Xi[1] < Xj[1]); }
+bool compareZ(vec3 Xi, vec3 Xj) { return (Xi[2] < Xj[2]); }
 
 double get_time() {
   struct timeval tv;
@@ -28,62 +24,61 @@ int main() {
   const int mpisize = 1000;
   double tic, toc;
   tic = get_time();
+  int * index = new int [numBodies];
+  Xvector xvector(numBodies);
   Bodies bodies(numBodies);
-  Index index(numBodies);
-  Positions positions(numBodies);
   Cells cells;
   toc = get_time();
   std::cout << "init : " << toc-tic << std::endl;
 
   tic = get_time();
   srand48(1);
-  for( B_iter B=bodies.begin(); B!=bodies.end(); ++B ) {
-    for( int d=0; d!=3; ++d ) B->X[d] = drand48();
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++) {
+    for (int d=0; d<3; d++) B->X[d] = drand48();
   }
   toc = get_time();
   std::cout << "rand : " << toc-tic << std::endl;
 
   tic = get_time();
-  P_iter P = positions.begin();
-  for( B_iter B=bodies.begin(); B!=bodies.end(); ++B, ++P ) {
-    P->X[0] = B->X[0];
-    P->X[1] = B->X[1];
-    P->X[2] = B->X[2];
+  X_iter X = xvector.begin();
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++, X++) {
+    X[0] = B->X[0];
+    X[1] = B->X[1];
+    X[2] = B->X[2];
   }
   toc = get_time();
   std::cout << "copy : " << toc-tic << std::endl;
 
   tic = get_time();
-  int level = int(log(mpisize)/M_LN2) + 1;
-  level = 0;
+  int level = 0;
   int size = mpisize - 1;
   while (size > 0) {
     size >>= 1;
     level++;
   }
   index[0] = 0;
-  index[1] = positions.size()/2;
-  index[2] = positions.size();
-  for( int l=0; l!=level; ++l ) {
+  index[1] = xvector.size()/2;
+  index[2] = xvector.size();
+  for (int l=0; l<level; l++) {
     std::cout << l << std::endl;
-    P_iter P0 = positions.begin();
-    if( l % 3 == 0 ) {
-      for( int i=0; i!=(1 << l); ++i ) {
-        std::nth_element(P0+index[2*i],P0+index[2*i+1],P0+index[2*i+2],compareX);
+    X_iter X0 = xvector.begin();
+    if (l % 3 == 0) {
+      for (int i=0; i<(1 << l); i++) {
+        std::nth_element(X0+index[2*i],X0+index[2*i+1],X0+index[2*i+2],compareX);
       }
     } else if( l % 3 == 1 ) {
-      for( int i=0; i!=(1 << l); ++i ) {
-        std::nth_element(P0+index[2*i],P0+index[2*i+1],P0+index[2*i+2],compareY);
+      for (int i=0; i<(1 << l); i++) {
+        std::nth_element(X0+index[2*i],X0+index[2*i+1],X0+index[2*i+2],compareY);
       }
     } else {
-      for( int i=0; i!=(1 << l); ++i ) {
-        std::nth_element(P0+index[2*i],P0+index[2*i+1],P0+index[2*i+2],compareZ);
+      for (int i=0; i<(1 << l); i++) {
+        std::nth_element(X0+index[2*i],X0+index[2*i+1],X0+index[2*i+2],compareZ);
       }
     }
-    for( int i=(2 << l); i>0; --i ) {
+    for (int i=(2 << l); i>0; i--) {
       index[2*i] = index[i];
     }
-    for( int i=(2 << l); i>0; --i ) {
+    for (int i=(2 << l); i>0; i--) {
       index[2*i-1] = (index[2*i-2] + index[2*i]) / 2;
     }
   }
@@ -91,14 +86,15 @@ int main() {
   std::cout << "sort : " << toc-tic << std::endl;
 
   tic = get_time();
-  for( int i=0; i!=(1 << level); ++i ) {
-    for( int b=index[2*i]; b!=index[2*i+2]; ++b ) {
-      bodies[b].X[0] = positions[b].X[0];
-      bodies[b].X[1] = positions[b].X[1];
-      bodies[b].X[2] = positions[b].X[2];
+  for (int i=0; i<(1 << level); i++) {
+    for (int b=index[2*i]; b<index[2*i+2]; b++) {
+      bodies[b].X[0] = xvector[b][0];
+      bodies[b].X[1] = xvector[b][1];
+      bodies[b].X[2] = xvector[b][2];
       bodies[b].IBODY = i;
     }
   }
   toc = get_time();
   std::cout << "set B: " << toc-tic << std::endl;
+  delete[] index;
 }
