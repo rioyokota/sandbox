0101eb7c674cee3256a4aecf06169407e9a9d5b8
Rio Yokota <rioyokota@me.com>
2014-09-24 15:33:16 +0300

    Clean tree.f: Step 4.

--- greengard/tree.f
+++ greengard/tree.f
@@ -138,6 +138,7 @@ c     store all pointers
         implicit real *8 (a-h,o-z)
         integer w(*),box(20)
         real *8 center(3),corners(3,8)
+        data nboxes/0/
 c
 c        this entry returns to the user the characteristics of
 c        user-specified box  ibox.  
@@ -225,7 +226,8 @@ c       return to the user the list number itype for the box ibox
 c
         iwlists=w(5)
 c
-        call d3tlinkretr(ier,itype,ibox,list,nlist,w(iwlists),lused)
+        call d3tlinkretr(ier,itype,ibox,nboxes,list,nlist,w(iwlists),
+     $       lused)
         return
         end
 c
@@ -315,7 +317,8 @@ c
         dadcolls(1)=idad
         itype5=5
         itype2=2
-        call d3tlinkretr(jer,itype5,idad,dadcolls(2),ncolls,w,lused)
+        call d3tlinkretr(jer,itype5,idad,nboxes,dadcolls(2),ncolls,w,
+     $       lused)
         ncolls=ncolls+1
 c
 c        find the children of the daddy's collegues
@@ -345,13 +348,13 @@ c
 ccc        call d3tifint2(boxes(1,kid),boxes(1,ibox),ifinter)
 c
         if(ifinter .eq. 1)
-     1    call d3tlinkstor(ier,itype5,ibox,kid,nlist1,w,lused)
+     1    call d3tlinkstor(ier,itype5,ibox,nboxes,kid,nlist1,w,lw,lused)
 c
 c        if storage capacity has been exceeed - bomb
 c
         if(ier .eq. 32) return
         if(ifinter .eq. 0)
-     1    call d3tlinkstor(ier,itype2,ibox,kid,nlist1,w,lused)
+     1    call d3tlinkstor(ier,itype2,ibox,nboxes,kid,nlist1,w,lw,lused)
  1800 continue
 c
 c        if storage capacity has been exceeed - bomb
@@ -373,14 +376,14 @@ c       do not construct lists 1, 3 for the main box
 c
         if(boxes(1,i) .eq. 0) goto 3000
 c
-        call d3tlinkretr(jer,itype5,i,list5,nlist,w,lused)  
+        call d3tlinkretr(jer,itype5,i,nboxes,list5,nlist,w,lused)  
 c
         if(jer .eq. 4) goto 3000
 c
         do 2200 j=1,nlist
         jbox=list5(j)
         call d3tlst31(ier,i,jbox,boxes,nboxes,
-     1    corners,w,stack,lused)
+     1    corners,w,stack,lw,lused)
 c
 c        if storage capacity has been exceeded - bomb
 c
@@ -399,9 +402,10 @@ c
         do 3600 ibox=1,nboxes
         do 2400 itype=1,5
 c
-        call d3tlinkretr(jer,itype,ibox,list5,nlist,w,lused)
+        call d3tlinkretr(jer,itype,ibox,nboxes,list5,nlist,w,lused)
         if(jer .eq. 4) goto 2400
-        call d3tlinkstor(ier,itype,ibox,list5,nlist,w(lused+1),lused2)
+        call d3tlinkstor(ier,itype,ibox,nboxes,list5,nlist,w(lused+1),
+     $       lw,lused2)
 c
 c        if storage capacity has been exceeed - bomb
 c
@@ -426,10 +430,11 @@ c
         nlist1=1
         do 4400 ibox=1,nboxes
 c
-        call d3tlinkretr(jer,itype3,ibox,list5,nlist,w,lused)
+        call d3tlinkretr(jer,itype3,ibox,nboxes,list5,nlist,w,lused)
         if(jer .eq. 4) goto 4400
         do 4200 j=1,nlist
-        call d3tlinkstor(ier,itype4,list5(j),ibox,nlist1,w,lused2)
+        call d3tlinkstor(ier,itype4,list5(j),nboxes,ibox,nlist1,w,
+     $          lw,lused2)
 c
 c        if storage capacity has been exceeed - bomb
 c
@@ -446,7 +451,7 @@ c
 c
 c
         subroutine d3tlst31(ier,ibox,jbox0,boxes,nboxes,
-     1    corners,w,stack,lused)
+     1    corners,w,stack,lw,lused)
         implicit real *8 (a-h,o-z)
         integer w(*)
         real *8 corners(3,8,*)
@@ -521,7 +526,7 @@ c
 ccc        call d3tifint2(boxes(1,ibox),boxes(1,jbox),ifinter)
 c
         if(ifinter .eq. 1) goto 2000
-        call d3tlinkstor(ier,itype3,ibox,jbox,nlist1,w,lused)
+        call d3tlinkstor(ier,itype3,ibox,nboxes,jbox,nlist1,w,lw,lused)
 c
 c        if storage capacity has been exceeed - bomb
 c
@@ -537,7 +542,7 @@ c       - enter it in list 1; enter this fact in the daddy's table;
 c       pass control to the daddy
 c       
         if(boxes(6,jbox) .ne. 0) goto 3000
-        call d3tlinkstor(ier,itype1,ibox,jbox,nlist1,w,lused)
+        call d3tlinkstor(ier,itype1,ibox,nboxes,jbox,nlist1,w,lw,lused)
 c
 c        if storage capacity has been exceeed - bomb
 c
@@ -548,7 +553,7 @@ c             is on the finer level than ibox - enter ibox
 c             in the list 1 of jbox
 c
         if(boxes(1,jbox) .eq. boxes(1,ibox)) goto 2400
-        call d3tlinkstor(ier,itype1,jbox,ibox,nlist1,w,lused)
+        call d3tlinkstor(ier,itype1,jbox,nboxes,ibox,nlist1,w,lw,lused)
 c
 c        if storage capacity has been exceeed - bomb
 c
@@ -1380,10 +1385,6 @@ c
            w(inums(i))=0
         enddo
 c
-c        w(3)=0
-        w(4)=nboxes
-        w(6)=lw
-
         do k=1,ntypes
            do i=1,nboxes
               w(i*ntypes+k+ilistadd)=-1
@@ -1394,7 +1395,7 @@ c
 c
 c
 c
-        entry d3tlinkstor(ier,itype,ibox,list,nlist,w,lused)
+        entry d3tlinkstor(ier,itype,ibox,nboxes,list,nlist,w,lw,lused)
 c
 c       this entry stores dynamically a list of positive numbers 
 c       in the storage array w.
@@ -1422,7 +1423,7 @@ c
 c       . . . if this storage request exceeds the available memory - bomb
 c
         ier=0
-        if(ilists+numele*2+nlist*2 .lt. w(6) ) goto 2200
+        if(ilists+numele*2+nlist*2 .lt. lw ) goto 2200
         ier=32
         return
  2200 continue
@@ -1431,7 +1432,7 @@ c       store the user-specified list in array w
 c
 
         call d3tlinksto0(itype,ibox,list,nlist,w(ilistadd),
-     1      w(4),w(ilists),numele,w(inums(itype)) )
+     1      nboxes,w(ilists),numele,w(inums(itype)) )
 c
 c       augment the amount of storage used 
 c        
@@ -1441,7 +1442,7 @@ c
 c
 c
 c
-        entry d3tlinkretr(ier,itype,ibox,list,nlist,w,lused)
+        entry d3tlinkretr(ier,itype,ibox,nboxes,list,nlist,w,lused)
 c
 c       this entry retrieves from the storage area  w 
 c       a list of positive numbers that has been stored there
@@ -1466,7 +1467,7 @@ c  lused - the number of integer elements used in array
 c          w after this call.
 c
         call d3tlinkret0(ier,itype,ibox,w(ilistadd),
-     1      w(ilists),list,w(4),nlist)
+     1      w(ilists),list,nboxes,nlist)
 c
         lused=ilists+numele*2+10
 c
