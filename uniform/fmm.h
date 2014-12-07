55afed2f6ac1df22927c72cd8878889d4d2793d8
Rio Yokota <rioyokota@me.com>
2014-12-07 05:48:22 +0300

    Adding back periodic: Step 3.

--- uniform/fmm.h
+++ uniform/fmm.h
@@ -14,10 +14,10 @@ private:
     }
     int numBucket = Imax - Imin + 1;
     int *bucket = new int [numBucket];
-    for( int i=0; i<numBucket; i++ ) bucket[i] = 0;
-    for( int i=0; i<numBodies; i++ ) bucket[key[i]-Imin]++;
-    for( int i=1; i<numBucket; i++ ) bucket[i] += bucket[i-1];
-    for( int i=numBodies-1; i>=0; --i ) {
+    for (int i=0; i<numBucket; i++) bucket[i] = 0;
+    for (int i=0; i<numBodies; i++) bucket[key[i]-Imin]++;
+    for (int i=1; i<numBucket; i++) bucket[i] += bucket[i-1];
+    for (int i=numBodies-1; i>=0; i--) {
       bucket[key[i]-Imin]--;
       int inew = bucket[key[i]-Imin];
       for_4d bodies2[inew][d] = bodies[i][d];
@@ -38,11 +38,11 @@ public:
     Multipole = new real [numCells][MTERM]();
     Local = new real [numCells][LTERM]();
     Leafs = new int [numLeafs][2]();
-    for( int i=0; i<numCells; i++ ) {
+    for (int i=0; i<numCells; i++) {
       for_m Multipole[i][m] = 0;
       for_l Local[i][l] = 0;
     }
-    for( int i=0; i<numLeafs; i++ ) {
+    for (int i=0; i<numLeafs; i++) {
       Leafs[i][0] = Leafs[i][1] = 0;
     }
   }
@@ -61,7 +61,7 @@ public:
     for_3d X0[d] = R0;
     srand48(0);
     real average = 0;
-    for( int i=0; i<numBodies; i++ ) {
+    for (int i=0; i<numBodies; i++) {
       Jbodies[i][0] = 2 * R0 * (drand48() + ix[0]);
       Jbodies[i][1] = 2 * R0 * (drand48() + ix[1]);
       Jbodies[i][2] = 2 * R0 * (drand48() + ix[2]);
@@ -69,7 +69,7 @@ public:
       average += Jbodies[i][3];
     }
     average /= numBodies;
-    for( int i=0; i<numBodies; i++ ) {
+    for (int i=0; i<numBodies; i++) {
       Jbodies[i][3] -= average;
     }
   }
@@ -78,12 +78,12 @@ public:
     int *key = new int [numBodies];
     real diameter = 2 * R0 / (1 << maxLevel);
     int ix[3] = {0, 0, 0};
-    for( int i=0; i<numBodies; i++ ) {
+    for (int i=0; i<numBodies; i++) {
       for_3d ix[d] = int((Jbodies[i][d] + R0 - X0[d]) / diameter);
       key[i] = getKey(ix,maxLevel);
     }
     sort(Jbodies,Ibodies,key);
-    for( int i=0; i<numBodies; i++ ) {
+    for (int i=0; i<numBodies; i++) {
       for_4d Jbodies[i][d] = Ibodies[i][d];
       for_4d Ibodies[i][d] = 0;
     }
@@ -96,17 +96,17 @@ public:
     for_3d ix[d] = int((Jbodies[0][d] + R0 - X0[d]) / diameter);
     int ileaf = getKey(ix,maxLevel,false);
     Leafs[ileaf][0] = 0;
-    for( int i=0; i<numBodies; i++ ) {
+    for (int i=0; i<numBodies; i++) {
       for_3d ix[d] = int((Jbodies[i][d] + R0 - X0[d]) / diameter);
       int inew = getKey(ix,maxLevel,false);
-      if( ileaf != inew ) {
+      if (ileaf != inew) {
         Leafs[ileaf][1] = Leafs[inew][0] = i;
         ileaf = inew;
       }
     }
     Leafs[ileaf][1] = numBodies;
-    for( int i=0; i<numLeafs; i++ ) {
-      if( Leafs[i][1] == Leafs[i][0] ) printf("Warning: Cell %d is empty.\n",i);
+    for (int i=0; i<numLeafs; i++) {
+      if (Leafs[i][1] == Leafs[i][0]) printf("Warning: Cell %d is empty.\n",i);
     }
   }
 
