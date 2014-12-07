5b02637a354660c1323f54871dab80c99f677a33
Rio Yokota <rioyokota@me.com>
2014-12-07 05:39:43 +0300

    Adding back periodic: Step 2.

--- uniform/kernels.h
+++ uniform/kernels.h
@@ -7,10 +7,10 @@ const int LTERM = (PP+1)*(PP+2)*(PP+3)/6;
 
 #include "core.h"
 
-#define for_3d for( int d=0; d<3; d++ )
-#define for_4d for( int d=0; d<4; d++ )
-#define for_m for( int m=0; m<MTERM; m++ )
-#define for_l for( int l=0; l<LTERM; l++ )
+#define for_3d for (int d=0; d<3; d++)
+#define for_4d for (int d=0; d<4; d++)
+#define for_m for (int m=0; m<MTERM; m++)
+#define for_l for (int l=0; l<LTERM; l++)
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
@@ -35,11 +35,11 @@ private:
   inline void getIndex(int *ix, int index) const {
     for_3d ix[d] = 0;
     int d = 0, level = 0;
-    while( index != 0 ) {
+    while (index != 0) {
       ix[d] += (index & 1) * (1 << level);
       index >>= 1;
       d = (d+1) % 3;
-      if( d == 0 ) level++;
+      if (d == 0) level++;
     }
   }
 
@@ -53,8 +53,8 @@ private:
 protected:
   inline int getKey(int *ix, int level, bool levelOffset=true) const {
     int id = 0;
-    if( levelOffset ) id = ((1 << 3 * level) - 1) / 7;
-    for( int lev=0; lev<level; ++lev ) {
+    if (levelOffset) id = ((1 << 3 * level) - 1) / 7;
+    for (int lev=0; lev<level; lev++) {
       for_3d id += ((ix[d] >> lev) & 1) << (3 * lev + d);
     }
     return id;
@@ -62,9 +62,9 @@ protected:
 
 public:
   void P2PSum(int ibegin, int iend, int jbegin, int jend, real *Xperiodic) const {
-    for( int i=ibegin; i<iend; i++ ) {
+    for (int i=ibegin; i<iend; i++) {
       real Po = 0, Fx = 0, Fy = 0, Fz = 0;
-      for( int j=jbegin; j<jend; j++ ) {
+      for (int j=jbegin; j<jend; j++) {
 	real dist[3];
 	for_3d dist[d] = Jbodies[i][d] - Jbodies[j][d] - Xperiodic[d];
 	real R2 = dist[0] * dist[0] + dist[1] * dist[1] + dist[2] * dist[2];
@@ -87,12 +87,12 @@ public:
     int nunit = 1 << maxLevel;
     int nmin = 0;
     int nmax = nunit - 1;
-    if( numImages != 0 ) {
+    if (numImages != 0) {
       nmin -= nunit;
       nmax += nunit;
     }
 #pragma omp parallel for
-    for( int i=0; i<numLeafs; i++ ) {
+    for (int i=0; i<numLeafs; i++) {
       int ix[3] = {0, 0, 0};
       getIndex(ix,i);
       int jxmin[3];
@@ -100,9 +100,9 @@ public:
       int jxmax[3];
       for_3d jxmax[d] = MIN(nmax, ix[d] + numNeighbors);
       int jx[3];
-      for( jx[2]=jxmin[2]; jx[2]<=jxmax[2]; jx[2]++ ) {
-        for( jx[1]=jxmin[1]; jx[1]<=jxmax[1]; jx[1]++ ) {
-          for( jx[0]=jxmin[0]; jx[0]<=jxmax[0]; jx[0]++ ) {
+      for (jx[2]=jxmin[2]; jx[2]<=jxmax[2]; jx[2]++) {
+        for (jx[1]=jxmin[1]; jx[1]<=jxmax[1]; jx[1]++) {
+          for (jx[0]=jxmin[0]; jx[0]<=jxmax[0]; jx[0]++) {
 	    int jxp[3];
 	    for_3d jxp[d] = (jx[d] + nunit) % nunit;
             int j = getKey(jxp,maxLevel,false);
@@ -119,10 +119,10 @@ public:
   void P2M() const {
     int levelOffset = ((1 << 3 * maxLevel) - 1) / 7;
 #pragma omp parallel for
-    for( int i=0; i<numLeafs; i++ ) {
+    for (int i=0; i<numLeafs; i++) {
       real center[3];
       getCenter(center,i,maxLevel);
-      for( int j=Leafs[i][0]; j<Leafs[i][1]; j++ ) {
+      for (int j=Leafs[i][0]; j<Leafs[i][1]; j++) {
         real dist[3];
         for_3d dist[d] = center[d] - Jbodies[j][d];
         real M[MTERM];
@@ -134,12 +134,12 @@ public:
   }
 
   void M2M() const {
-    for( int lev=maxLevel; lev>0; lev-- ) {
+    for (int lev=maxLevel; lev>0; lev--) {
       int childOffset = ((1 << 3 * lev) - 1) / 7;
       int parentOffset = ((1 << 3 * (lev - 1)) - 1) / 7;
       real radius = R0 / (1 << lev);
 #pragma omp parallel for schedule(static, 8)
-      for( int i=0; i<(1 << 3 * lev); i++ ) {
+      for (int i=0; i<(1 << 3 * lev); i++) {
         int c = i + childOffset;
         int p = (i >> 3) + parentOffset;
         int ix[3];
@@ -158,7 +158,7 @@ public:
   }
 
   void M2L() const {
-    for( int lev=1; lev<=maxLevel; lev++ ) {
+    for (int lev=1; lev<=maxLevel; lev++) {
       int levelOffset = ((1 << 3 * lev) - 1) / 7;
       int nunit = 1 << lev;
       real diameter = 2 * R0 / (1 << lev);
@@ -169,7 +169,7 @@ public:
 	nmax += (nunit >> 1);
       }
 #pragma omp parallel for
-      for( int i=0; i<(1 << 3 * lev); i++ ) {
+      for (int i=0; i<(1 << 3 * lev); i++) {
         real L[LTERM];
         for_l L[l] = 0;
         int ix[3] = {0, 0, 0};
@@ -179,9 +179,9 @@ public:
         int jxmax[3];
         for_3d jxmax[d] = (MIN(nmax, (ix[d] >> 1) + numNeighbors) << 1) + 1;
         int jx[3];
-        for( jx[2]=jxmin[2]; jx[2]<=jxmax[2]; jx[2]++ ) {
-          for( jx[1]=jxmin[1]; jx[1]<=jxmax[1]; jx[1]++ ) {
-            for( jx[0]=jxmin[0]; jx[0]<=jxmax[0]; jx[0]++ ) {
+        for (jx[2]=jxmin[2]; jx[2]<=jxmax[2]; jx[2]++) {
+          for (jx[1]=jxmin[1]; jx[1]<=jxmax[1]; jx[1]++) {
+            for (jx[0]=jxmin[0]; jx[0]<=jxmax[0]; jx[0]++) {
               if(jx[0] < ix[0]-numNeighbors || ix[0]+numNeighbors < jx[0] ||
                  jx[1] < ix[1]-numNeighbors || ix[1]+numNeighbors < jx[1] ||
                  jx[2] < ix[2]-numNeighbors || ix[2]+numNeighbors < jx[2]) {
@@ -205,12 +205,12 @@ public:
   }
 
   void L2L() const {
-    for( int lev=1; lev<=maxLevel; lev++ ) {
+    for (int lev=1; lev<=maxLevel; lev++) {
       int childOffset = ((1 << 3 * lev) - 1) / 7;
       int parentOffset = ((1 << 3 * (lev - 1)) - 1) / 7;
       real radius = R0 / (1 << lev);
 #pragma omp parallel for
-      for( int i=0; i<(1 << 3 * lev); i++ ) {
+      for (int i=0; i<(1 << 3 * lev); i++) {
         int c = i + childOffset;
         int p = (i >> 3) + parentOffset;
         int ix[3];
@@ -223,7 +223,7 @@ public:
         C[0] = 1;
         powerL(C,dist);
         for_l Local[c][l] += Local[p][l];
-        for( int l=1; l<LTERM; l++ ) Local[c][0] += C[l] * Local[p][l];
+        for (int l=1; l<LTERM; l++) Local[c][0] += C[l] * Local[p][l];
         L2LSum(Local[c],C,Local[p]);
       }
     }
@@ -232,22 +232,21 @@ public:
   void L2P() const {
     int levelOffset = ((1 << 3 * maxLevel) - 1) / 7;
 #pragma omp parallel for
-    for( int i=0; i<numLeafs; i++ ) {
+    for (int i=0; i<numLeafs; i++) {
       real center[3];
       getCenter(center,i,maxLevel);
       real L[LTERM];
       for_l L[l] = Local[i+levelOffset][l];
-      for( int j=Leafs[i][0]; j<Leafs[i][1]; j++ ) {
+      for (int j=Leafs[i][0]; j<Leafs[i][1]; j++) {
         real dist[3];
         for_3d dist[d] = Jbodies[j][d] - center[d];
         real C[LTERM];
         C[0] = 1;
         powerL(C,dist);
         for_4d Ibodies[j][d] += L[d];
-        for( int l=1; l<LTERM; l++ ) Ibodies[j][0] += C[l] * L[l];
+        for (int l=1; l<LTERM; l++) Ibodies[j][0] += C[l] * L[l];
         L2PSum(Ibodies[j],C,L);
       }
     }
   }
-
 };
