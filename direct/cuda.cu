d483488ebec3daea818593cdb0cad4cc1d506413
Rio Yokota <rioyokota@me.com>
2015-01-29 17:26:31 +0300

    Use real_t in direct/cuda.cu.

--- direct/cuda.cu
+++ direct/cuda.cu
@@ -6,6 +6,7 @@
 #include <xmmintrin.h>
 
 #define THREADS 512
+typedef float real_t;
 
 double get_time() {
   struct timeval tv;
@@ -14,17 +15,17 @@ double get_time() {
   return (double)(tv.tv_sec+tv.tv_usec*1e-6);
 }
 
-__global__ void GPUkernel(int N, float * x, float * y, float * z, float * m,
-			  float * p, float * ax, float * ay, float * az, float eps2) {
+__global__ void GPUkernel(int N, real_t * x, real_t * y, real_t * z, real_t * m,
+			  real_t * p, real_t * ax, real_t * ay, real_t * az, real_t eps2) {
   int i = blockIdx.x * THREADS + threadIdx.x;
-  float pi = 0;
-  float axi = 0;
-  float ayi = 0;
-  float azi = 0;
-  float xi = x[i];
-  float yi = y[i];
-  float zi = z[i];
-  __shared__ float xj[THREADS], yj[THREADS], zj[THREADS], mj[THREADS];
+  real_t pi = 0;
+  real_t axi = 0;
+  real_t ayi = 0;
+  real_t azi = 0;
+  real_t xi = x[i];
+  real_t yi = y[i];
+  real_t zi = z[i];
+  __shared__ real_t xj[THREADS], yj[THREADS], zj[THREADS], mj[THREADS];
   for ( int jb=0; jb<N/THREADS; jb++ ) {
     __syncthreads();
     xj[threadIdx.x] = x[jb*THREADS+threadIdx.x];
@@ -34,13 +35,13 @@ __global__ void GPUkernel(int N, float * x, float * y, float * z, float * m,
     __syncthreads();
 #pragma unroll
     for( int j=0; j<THREADS; j++ ) {
-      float dx = xj[j] - xi;
-      float dy = yj[j] - yi;
-      float dz = zj[j] - zi;
-      float R2 = dx * dx + dy * dy + dz * dz + eps2;
-      float invR = rsqrtf(R2);
+      real_t dx = xj[j] - xi;
+      real_t dy = yj[j] - yi;
+      real_t dz = zj[j] - zi;
+      real_t R2 = dx * dx + dy * dy + dz * dz + eps2;
+      real_t invR = rsqrtf(R2);
       pi += mj[j] * invR;
-      float invR3 = mj[j] * invR * invR * invR;
+      real_t invR3 = mj[j] * invR * invR * invR;
       axi += dx * invR3;
       ayi += dy * invR3;
       azi += dz * invR3;
@@ -56,17 +57,17 @@ int main() {
 // Initialize
   int N = 1 << 16;
   int i, j;
-  float OPS = 20. * N * N * 1e-9;
-  float EPS2 = 1e-6;
+  real_t OPS = 20. * N * N * 1e-9;
+  real_t EPS2 = 1e-6;
   double tic, toc;
-  float * x = (float*) malloc(N * sizeof(float));
-  float * y = (float*) malloc(N * sizeof(float));
-  float * z = (float*) malloc(N * sizeof(float));
-  float * m = (float*) malloc(N * sizeof(float));
-  float * p = (float*) malloc(N * sizeof(float));
-  float * ax = (float*) malloc(N * sizeof(float));
-  float * ay = (float*) malloc(N * sizeof(float));
-  float * az = (float*) malloc(N * sizeof(float));
+  real_t * x = (real_t*) malloc(N * sizeof(real_t));
+  real_t * y = (real_t*) malloc(N * sizeof(real_t));
+  real_t * z = (real_t*) malloc(N * sizeof(real_t));
+  real_t * m = (real_t*) malloc(N * sizeof(real_t));
+  real_t * p = (real_t*) malloc(N * sizeof(real_t));
+  real_t * ax = (real_t*) malloc(N * sizeof(real_t));
+  real_t * ay = (real_t*) malloc(N * sizeof(real_t));
+  real_t * az = (real_t*) malloc(N * sizeof(real_t));
   for (i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
@@ -83,22 +84,22 @@ int main() {
 
 // CUDA
   tic = get_time();
-  float *x_d, *y_d, *z_d, *m_d, *p_d, *ax_d, *ay_d, *az_d;
-  cudaMalloc((void**)&x_d, N * sizeof(float));
-  cudaMalloc((void**)&y_d, N * sizeof(float));
-  cudaMalloc((void**)&z_d, N * sizeof(float));
-  cudaMalloc((void**)&m_d, N * sizeof(float));
-  cudaMalloc((void**)&p_d, N * sizeof(float));
-  cudaMalloc((void**)&ax_d, N * sizeof(float));
-  cudaMalloc((void**)&ay_d, N * sizeof(float));
-  cudaMalloc((void**)&az_d, N * sizeof(float));
+  real_t *x_d, *y_d, *z_d, *m_d, *p_d, *ax_d, *ay_d, *az_d;
+  cudaMalloc((void**)&x_d, N * sizeof(real_t));
+  cudaMalloc((void**)&y_d, N * sizeof(real_t));
+  cudaMalloc((void**)&z_d, N * sizeof(real_t));
+  cudaMalloc((void**)&m_d, N * sizeof(real_t));
+  cudaMalloc((void**)&p_d, N * sizeof(real_t));
+  cudaMalloc((void**)&ax_d, N * sizeof(real_t));
+  cudaMalloc((void**)&ay_d, N * sizeof(real_t));
+  cudaMalloc((void**)&az_d, N * sizeof(real_t));
   toc = get_time();
   //printf("malloc : %e s\n",toc-tic);
   tic = get_time();
-  cudaMemcpy(x_d, x, N * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(y_d, y, N * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(z_d, z, N * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(m_d, m, N * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(x_d, x, N * sizeof(real_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(y_d, y, N * sizeof(real_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(z_d, z, N * sizeof(real_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(m_d, m, N * sizeof(real_t), cudaMemcpyHostToDevice);
   toc = get_time();
   //printf("memcpy : %e s\n",toc-tic);
   PAPI_start(EventSet);
@@ -109,10 +110,10 @@ int main() {
   printf("L2 Miss: %lld L2 Access: %lld TLB Miss: %lld\n",values[0],values[1],values[2]);
   printf("CUDA   : %e s : %lf GFlops\n",toc-tic, OPS/(toc-tic));
   tic = get_time();
-  cudaMemcpy(p, p_d, N * sizeof(float), cudaMemcpyDeviceToHost);
-  cudaMemcpy(ax, ax_d, N * sizeof(float), cudaMemcpyDeviceToHost);
-  cudaMemcpy(ay, ay_d, N * sizeof(float), cudaMemcpyDeviceToHost);
-  cudaMemcpy(az, az_d, N * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(p, p_d, N * sizeof(real_t), cudaMemcpyDeviceToHost);
+  cudaMemcpy(ax, ax_d, N * sizeof(real_t), cudaMemcpyDeviceToHost);
+  cudaMemcpy(ay, ay_d, N * sizeof(real_t), cudaMemcpyDeviceToHost);
+  cudaMemcpy(az, az_d, N * sizeof(real_t), cudaMemcpyDeviceToHost);
   toc = get_time();
   //printf("memcpy : %e s\n",toc-tic);
   cudaFree(x_d);
@@ -126,25 +127,25 @@ int main() {
   for (i=0; i<3; i++) values[i] = 0;
 
 // No CUDA
-  float pdiff = 0, pnorm = 0, adiff = 0, anorm = 0;
+  real_t pdiff = 0, pnorm = 0, adiff = 0, anorm = 0;
   PAPI_start(EventSet);
   tic = get_time();
 #pragma omp parallel for private(j) reduction(+: pdiff, pnorm, adiff, anorm)
   for (i=0; i<N; i++) {
-    float pi = 0;
-    float axi = 0;
-    float ayi = 0;
-    float azi = 0;
-    float xi = x[i];
-    float yi = y[i];
-    float zi = z[i];
+    real_t pi = 0;
+    real_t axi = 0;
+    real_t ayi = 0;
+    real_t azi = 0;
+    real_t xi = x[i];
+    real_t yi = y[i];
+    real_t zi = z[i];
     for (j=0; j<N; j++) {
-      float dx = x[j] - xi;
-      float dy = y[j] - yi;
-      float dz = z[j] - zi;
-      float R2 = dx * dx + dy * dy + dz * dz + EPS2;
-      float invR = 1.0f / sqrtf(R2);
-      float invR3 = m[j] * invR * invR * invR;
+      real_t dx = x[j] - xi;
+      real_t dy = y[j] - yi;
+      real_t dz = z[j] - zi;
+      real_t R2 = dx * dx + dy * dy + dz * dz + EPS2;
+      real_t invR = 1.0f / sqrtf(R2);
+      real_t invR3 = m[j] * invR * invR * invR;
       pi += m[j] * invR;
       axi += dx * invR3;
       ayi += dy * invR3;
