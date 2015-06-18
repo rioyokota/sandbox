23f8f1e4f8cb055a6b59373a87db3595f8680302
Rio Yokota <rioyokota@me.com>
2015-06-18 11:44:26 +0900

    Clean kmeans.cxx: Step 1.

--- fmm/kmeans.cxx
+++ fmm/kmeans.cxx
@@ -1,8 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
 
-typedef struct { double x, y; int group; } point_t, *point;
+struct Body {
+  double x, y;
+  int group;
+};
+typedef std::vector<Body> Bodies;
+typedef Body* point;
 
 double randf(double m)
 {
@@ -12,7 +18,7 @@ double randf(double m)
 point gen_xy(int count, double radius)
 {
   double ang, r;
-  point p, pt = (point) malloc(sizeof(point_t) * count);
+  point p, pt = (point) malloc(sizeof(Body) * count);
 
   /* note: this is not a uniform 2-d distribution */
   for (p = pt + count; p-- > pt;) {
@@ -38,11 +44,10 @@ nearest(point pt, point cent, int n_cluster, double *d2)
   point c;
   double d, min_d;
 
-#define for_n for (c = cent, i = 0; i < n_cluster; i++, c++)
-  for_n {
+  for (c = cent, i = 0; i < n_cluster; i++, c++) {
     min_d = HUGE_VAL;
     min_i = pt->group;
-    for_n {
+    for (c = cent, i = 0; i < n_cluster; i++, c++) {
       if (min_d > (d = dist2(c, pt))) {
 	min_d = d; min_i = i;
       }
@@ -83,7 +88,7 @@ point lloyd(point pts, int len, int n_cluster)
   int i, j, min_i;
   int changed;
 
-  point cent = (point) malloc(sizeof(point_t) * n_cluster), p, c;
+  point cent = (point) malloc(sizeof(Body) * n_cluster), p, c;
 
   /* assign init grouping randomly */
   //for_len p->group = j % n_cluster;
@@ -93,13 +98,13 @@ point lloyd(point pts, int len, int n_cluster)
 
   do {
     /* group element for centroids are used as counters */
-    for_n { c->group = 0; c->x = c->y = 0; }
+    for (c = cent, i = 0; i < n_cluster; i++, c++) { c->group = 0; c->x = c->y = 0; }
     for_len {
       c = cent + p->group;
       c->group++;
       c->x += p->x; c->y += p->y;
     }
-    for_n { c->x /= c->group; c->y /= c->group; }
+    for (c = cent, i = 0; i < n_cluster; i++, c++) { c->x /= c->group; c->y /= c->group; }
 
     changed = 0;
     /* find closest centroid of each point */
@@ -112,53 +117,24 @@ point lloyd(point pts, int len, int n_cluster)
     }
   } while (changed > (len >> 10)); /* stop when 99.9% of points are good */
 
-  for_n { c->group = i; }
+  for (c = cent, i = 0; i < n_cluster; i++, c++) { c->group = i; }
 
   return cent;
 }
 
 void print_eps(point pts, int len, point cent, int n_cluster)
 {
-  #define W 400
-  #define H 400
   int i, j;
-  point p, c;
-  double min_x, max_x, min_y, max_y, scale, cx, cy;
-  double *colors = (double*) malloc(sizeof(double) * n_cluster * 3);
-
-  for_n {
-    colors[3*i + 0] = (3 * (i + 1) % 11)/11.;
-    colors[3*i + 1] = (7 * i % 11)/11.;
-    colors[3*i + 2] = (9 * i % 11)/11.;
-  }
-
-  max_x = max_y = -(min_x = min_y = HUGE_VAL);
-  for_len {
-    if (max_x < p->x) max_x = p->x;
-    if (min_x > p->x) min_x = p->x;
-    if (max_y < p->y) max_y = p->y;
-    if (min_y > p->y) min_y = p->y;
-  }
-  scale = W / (max_x - min_x);
-  if (scale > H / (max_y - min_y)) scale = H / (max_y - min_y);
-  cx = (max_x + min_x) / 2;
-  cy = (max_y + min_y) / 2;
-
+  point p;
   FILE * fid = fopen("kmeans.dat","w");
-  for_n {
-    fprintf(fid, "%d %g %g\n", i,
-	   (c->x - cx) * scale + W / 2,
-	   (c->y - cy) * scale + H / 2);
+  for (point c = cent; c < cent+n_cluster; c++) {
+    fprintf(fid, "%d %g %g\n", c-cent, c->x, c->y);
     for_len {
-      if (p->group != i) continue;
-      fprintf(fid, "%d %g %g\n", i,
-	     (p->x - cx) * scale + W / 2,
-	     (p->y - cy) * scale + H / 2);
+      if (p->group != c-cent) continue;
+      fprintf(fid, "%d %g %g\n", c-cent, p->x, p->y);
     }
   }
   fclose(fid);
-  free(colors);
-  #undef for_n
   #undef for_len
 }
 
@@ -170,6 +146,6 @@ int main()
   point v = gen_xy(PTS, 10);
   point c = lloyd(v, PTS, K);
   print_eps(v, PTS, c, K);
-  // free(v); free(c);
+  free(v); free(c);
   return 0;
 }
