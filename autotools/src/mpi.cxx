82e95cbb5056a91fca6144e423de28c0cf9803bb
Rio Yokota <rioyokota@gmail.com>
2015-09-19 22:06:16 +0900

    c++ working version

--- autotools/src/mpi.cxx
+++ autotools/src/mpi.cxx
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <iostream>
 int main(int argc, char ** argv) {
+  int rank;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   std::cout << "myrank: " << rank << std::endl;
