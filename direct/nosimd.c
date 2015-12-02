ce30c53dd3acc74139a57b6300ee1ee6c773299e
Rio Yokota <rioyokota@gmail.com>
2015-12-03 06:32:36 +0900

    Adding nosimd.c

---
+++ direct/nosimd.c
@@ -0,0 +1,76 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+double get_time() {
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return (double)(tv.tv_sec+tv.tv_usec*1e-6);
+}
+
+int main() {
+// Initialize
+  int N = 1 << 16;
+  int i, j;
+  float OPS = 20. * N * N * 1e-9;
+  float EPS2 = 1e-6;
+  double tic, toc;
+  float * x = (float*) malloc(N * sizeof(float));
+  float * y = (float*) malloc(N * sizeof(float));
+  float * z = (float*) malloc(N * sizeof(float));
+  float * m = (float*) malloc(N * sizeof(float));
+  float * p = (float*) malloc(N * sizeof(float));
+  float * ax = (float*) malloc(N * sizeof(float));
+  float * ay = (float*) malloc(N * sizeof(float));
+  float * az = (float*) malloc(N * sizeof(float));
+  for (i=0; i<N; i++) {
+    x[i] = drand48();
+    y[i] = drand48();
+    z[i] = drand48();
+    m[i] = drand48() / N;
+  }
+  printf("N      : %d\n",N);
+
+// No SSE
+  tic = get_time();
+#pragma omp parallel for private(j)
+  for (i=0; i<N; i++) {
+    float pi = 0;
+    float axi = 0;
+    float ayi = 0;
+    float azi = 0;
+    float xi = x[i];
+    float yi = y[i];
+    float zi = z[i];
+    for (j=0; j<N; j++) {
+      float dx = x[j] - xi;
+      float dy = y[j] - yi;
+      float dz = z[j] - zi;
+      float R2 = dx * dx + dy * dy + dz * dz + EPS2;
+      float invR = 1.0f / sqrtf(R2);
+      float invR3 = m[j] * invR * invR * invR;
+      pi += m[j] * invR;
+      axi += dx * invR3;
+      ayi += dy * invR3;
+      azi += dz * invR3;
+    }
+    p[i] = pi;
+    ax[i] = axi;
+    ay[i] = ayi;
+    az[i] = azi;
+  }
+  toc = get_time();
+  printf("No SSE : %e s : %lf GFlops\n",toc-tic, OPS/(toc-tic));
+
+// DEALLOCATE
+  free(x);
+  free(y);
+  free(z);
+  free(m);
+  free(p);
+  free(ax);
+  free(ay);
+  free(az);
+  return 0;
+}
