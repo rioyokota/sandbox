dc5d9d5d010ff307c04f21b600369b1e7f1563a2
Rio Yokota <rioyokota@gmail.com>
2016-02-23 08:40:01 +0900

    exafmm2d: Remove TBB: Step 6.

--- exafmm2d/test.cxx
+++ exafmm2d/test.cxx
@@ -2,7 +2,6 @@
 
 #include "boundbox.h"
 #include "buildtree.h"
-#include "dataset.h"
 #include "logger.h"
 #include "traversal.h"
 #include "updownpass.h"
@@ -23,7 +22,16 @@ int main(int argc, char ** argv) {
   Traversal traversal(nspawn,images,eps2);
   logger.printTitle("FMM Profiling");
   logger.startTimer("Total FMM");
-  Bodies bodies = initBodies(numBodies);
+//! Initialize dsitribution, source & target value of bodies
+  srand48(0);                                                 // Set seed for random number generator
+  Bodies bodies(numBodies);                                   // Initialize bodies
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++) {       // Loop over bodies
+    for (int d=0; d<2; d++) {                                 //  Loop over dimension
+      B->X[d] = drand48() * 2 * M_PI - M_PI;                  //   Initialize positions
+    }                                                         //  End loop over dimension
+    B->SRC = drand48() - .5;                                  //   Initialize charge
+    B->TRG = 0;                                               //  Clear target values
+  }                                                           // End loop over bodies
   Bounds bounds = boundbox.getBounds(bodies);
   Cells cells = tree.buildTree(bodies, bounds);
   pass.upwardPass(cells);
@@ -32,8 +40,14 @@ int main(int argc, char ** argv) {
   pass.downwardPass(cells);
   logger.printTitle("Total runtime");
   logger.stopTimer("Total FMM");
-  sampleBodies(bodies, 10);
-  Bodies bodies2 = bodies;
+//! Downsize target bodies by even sampling 
+  int numTargets = 10;                                          // Number of target bodies
+  int stride = bodies.size() / numTargets;                      // Stride of sampling
+  for (int i=0; i<numTargets; i++) {                            // Loop over target samples
+    bodies[i] = bodies[i*stride];                               //  Sample targets
+  }                                                             // End loop over target samples
+  bodies.resize(numTargets);                                    // Resize bodies to target size
+  Bodies bodies2 = bodies;                                      // Save bodies in bodies2
   for (B_iter B=bodies.begin(); B!=bodies.end(); B++) {         // Loop over bodies
     B->TRG = 0;                                                 //  Clear target values
   }                                                             // End loop over bodies
@@ -42,7 +56,14 @@ int main(int argc, char ** argv) {
   traversal.normalize(bodies);
   logger.stopTimer("Total Direct");
   double diff1 = 0, norm1 = 0;
-  evalError(bodies2, bodies, diff1, norm1);
+//! Evaluate relaitve L2 norm error
+  B_iter B2 = bodies2.begin();                                // Set iterator for bodies2
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++, B2++) { // Loop over bodies & bodies2
+    double dp = (B->TRG - B2->TRG) * (B->TRG - B2->TRG);      // Difference of potential
+    double  p = B2->TRG * B2->TRG;                            //  Value of potential
+    diff1 += dp;                                              //  Accumulate difference of potential
+    norm1 += p;                                               //  Accumulate value of potential
+  }                                                           // End loop over bodies & bodies2
   logger.printTitle("FMM vs. direct");
   logger.printError(diff1, norm1);
   return 0;
