60ce0e1a4fe98725637cd62b96b3a627ea2285b3
Rio Yokota <rioyokota@gmail.com>
2016-03-03 09:31:05 +0900

    Merge headers: Step 33.

--- pvfmm/include/mem_mgr.hpp
+++ pvfmm/include/mem_mgr.hpp
@@ -1,7 +1,10 @@
+#include <cmath>
 #include <omp.h>
 #include <cstdlib>
+#include <cstring>
 #include <stdint.h>
 #include <cassert>
+#include <iostream>
 #include <vector>
 #include <stack>
 #include <map>
@@ -88,25 +91,181 @@ public:
     omp_init_lock(&omp_lock);
   }
 
-  ~MemoryManager();
+  ~MemoryManager(){
+    MemNode* n_dummy=&node_buff[n_dummy_indx-1];
+    MemNode* n=&node_buff[n_dummy->next-1];
+    if(!n->free || n->size!=buff_size ||
+       node_stack.size()!=node_buff.size()-2){
+      std::cout<<"\nWarning: memory leak detected.\n";
+    }
+    omp_destroy_lock(&omp_lock);
 
-  static inline MemHead* GetMemHead(void* p);
+    { // Check out-of-bounds write
+#ifndef NDEBUG
+#pragma omp parallel for
+      for(size_t i=0;i<buff_size;i++){
+	assert(buff[i]==init_mem_val);
+      }
+#endif
+    }
+    { // free buff
+      assert(buff);
+      std::free(buff-((uint16_t*)buff)[-1]);
+    }
+    }
 
-  void* malloc(const size_t n_elem=1, const size_t type_size=sizeof(char)) const;
+  static inline MemHead* GetMemHead(void* p){
+    static uintptr_t alignment=MEM_ALIGN-1;
+    static uintptr_t header_size=(uintptr_t)(sizeof(MemoryManager::MemHead)+alignment) & ~(uintptr_t)alignment;
+    return (MemHead*)(((char*)p)-header_size);
+  }
 
-  void free(void* p) const;
+  void* malloc(const size_t n_elem=1, const size_t type_size=sizeof(char)) const{
+    if(!n_elem) return NULL;
+    static uintptr_t alignment=MEM_ALIGN-1;
+    static uintptr_t header_size=(uintptr_t)(sizeof(MemHead)+alignment) & ~(uintptr_t)alignment;
 
-  void print() const;
+    size_t size=n_elem*type_size+header_size;
+    size=(uintptr_t)(size+alignment) & ~(uintptr_t)alignment;
+    char* base=NULL;
 
-  static void test();
+    omp_set_lock(&omp_lock);
+    std::multimap<size_t, size_t>::iterator it=free_map.lower_bound(size);
+    size_t n_indx=(it!=free_map.end()?it->second:0);
+    if(n_indx){ // Allocate from buff
+      size_t n_free_indx=(it->first>size?new_node():0);
+      MemNode& n=node_buff[n_indx-1];
+      assert(n.size==it->first);
+      assert(n.it==it);
+      assert(n.free);
 
-  void Check() const;
+      if(n_free_indx){ // Create a node for the remaining free part.
+	MemNode& n_free=node_buff[n_free_indx-1];
+	n_free=n;
+	n_free.size-=size;
+	n_free.mem_ptr=(char*)n_free.mem_ptr+size;
+	{ // Insert n_free to the link list
+	  n_free.prev=n_indx;
+	  if(n_free.next){
+	    size_t n_next_indx=n_free.next;
+	    MemNode& n_next=node_buff[n_next_indx-1];
+	    n_next.prev=n_free_indx;
+	  }
+	  n.next=n_free_indx;
+	}
+	assert(n_free.free); // Insert n_free to free map
+	n_free.it=free_map.insert(std::make_pair(n_free.size,n_free_indx));
+	n.size=size; // Update n
+      }
+
+      n.free=false;
+      free_map.erase(it);
+      base = n.mem_ptr;
+    }
+    omp_unset_lock(&omp_lock);
+    if(!base){ // Use system malloc
+      size+=2+alignment;
+      char* p = (char*)std::malloc(size);
+      base = (char*)((uintptr_t)(p+2+alignment) & ~(uintptr_t)alignment);
+      ((uint16_t*)base)[-1] = (uint16_t)(base-p);
+    }
+
+    { // Check out-of-bounds write
+#ifndef NDEBUG
+      if(n_indx){
+#pragma omp parallel for
+      for(size_t i=0;i<size;i++) assert(base[i]==init_mem_val);
+    }
+#endif
+  }
+
+    MemHead* mem_head=(MemHead*)base;
+    { // Set mem_head
+    mem_head->n_indx=n_indx;
+    mem_head->n_elem=n_elem;
+    mem_head->type_size=type_size;
+  }
+    { // Set header check_sum
+#ifndef NDEBUG
+    size_t check_sum=0;
+    mem_head->check_sum=0;
+    for(size_t i=0;i<header_size;i++){
+      check_sum+=base[i];
+    }
+    check_sum=check_sum & ((1UL << sizeof(mem_head->check_sum))-1);
+    mem_head->check_sum=check_sum;
+#endif
+  }
+    return (void*)(base+header_size);
+  }
+
+  void free(void* p) const{
+    if(!p) return;
+    static uintptr_t alignment=MEM_ALIGN-1;
+    static uintptr_t header_size=(uintptr_t)(sizeof(MemHead)+alignment) & ~(uintptr_t)alignment;
+
+    char* base=(char*)((char*)p-header_size);
+    MemHead* mem_head=(MemHead*)base;
+
+    if(base<&buff[0] || base>=&buff[buff_size]){ // Use system free
+      char* p_=(char*)((uintptr_t)base-((uint16_t*)base)[-1]);
+      return std::free(p_);
+    }
+
+    size_t n_indx=mem_head->n_indx;
+    assert(n_indx>0 && n_indx<=node_buff.size());
+    { // Verify header check_sum; set array to init_mem_val
+#ifndef NDEBUG
+      { // Verify header check_sum
+      size_t check_sum=0;
+      for(size_t i=0;i<header_size;i++){
+      check_sum+=base[i];
+    }
+      check_sum-=mem_head->check_sum;
+      check_sum=check_sum & ((1UL << sizeof(mem_head->check_sum))-1);
+      assert(check_sum==mem_head->check_sum);
+    }
+      size_t size=mem_head->n_elem*mem_head->type_size;
+#pragma omp parallel for
+      for(size_t i=0;i<size;i++) ((char*)p)[i]=init_mem_val;
+      for(size_t i=0;i<sizeof(MemHead);i++) base[i]=init_mem_val;
+#endif
+    }
+
+    omp_set_lock(&omp_lock);
+    MemNode& n=node_buff[n_indx-1];
+    assert(!n.free && n.size>0 && n.mem_ptr==base);
+    if(n.prev!=0 && node_buff[n.prev-1].free){
+      size_t n_prev_indx=n.prev;
+      MemNode& n_prev=node_buff[n_prev_indx-1];
+      n.size+=n_prev.size;
+      n.mem_ptr=n_prev.mem_ptr;
+      n.prev=n_prev.prev;
+      free_map.erase(n_prev.it);
+      delete_node(n_prev_indx);
+
+      if(n.prev){
+      node_buff[n.prev-1].next=n_indx;
+    }
+    }
+    if(n.next!=0 && node_buff[n.next-1].free){
+      size_t n_next_indx=n.next;
+      MemNode& n_next=node_buff[n_next_indx-1];
+      n.size+=n_next.size;
+      n.next=n_next.next;
+      free_map.erase(n_next.it);
+      delete_node(n_next_indx);
+
+      if(n.next){
+      node_buff[n.next-1].prev=n_indx;
+    }
+    }
+    n.free=true; // Insert n to free_map
+    n.it=free_map.insert(std::make_pair(n.size,n_indx));
+    omp_unset_lock(&omp_lock);
+    }
 
 private:
-
-  MemoryManager();
-
-  MemoryManager(const MemoryManager& m);
 
   struct MemNode{
     bool free;
@@ -116,9 +275,28 @@ private:
     std::multimap<size_t, size_t>::iterator it;
   };
 
-  inline size_t new_node() const;
+  inline size_t new_node() const{
+    if(node_stack.empty()){
+      node_buff.resize(node_buff.size()+1);
+      node_stack.push(node_buff.size());
+    }
+    size_t indx=node_stack.top();
+    node_stack.pop();
+    assert(indx);
+    return indx;
+  }
 
-  inline void delete_node(size_t indx) const;
+  inline void delete_node(size_t indx) const{
+    assert(indx);
+    assert(indx<=node_buff.size());
+    MemNode& n=node_buff[indx-1];
+    n.free=false;
+    n.size=0;
+    n.prev=0;
+    n.next=0;
+    n.mem_ptr=NULL;
+    node_stack.push(indx);
+  }
 
   char* buff;
   size_t buff_size;
@@ -130,7 +308,7 @@ private:
   mutable omp_lock_t omp_lock;
 };
 
-  extern MemoryManager glbMemMgr;
+  MemoryManager glbMemMgr(GLOBAL_MEM_BUFF*1024LL*1024LL);
 
   inline uintptr_t align_ptr(uintptr_t ptr){
     static uintptr_t     ALIGN_MINUS_ONE=MEM_ALIGN-1;
@@ -139,16 +317,66 @@ private:
   }
 
   template <class T>
-  inline T* aligned_new(size_t n_elem=1, const MemoryManager* mem_mgr=&glbMemMgr);
+  inline T* aligned_new(size_t n_elem=1, const MemoryManager* mem_mgr=&glbMemMgr) {
+    if(!n_elem) return NULL;
+
+    static MemoryManager def_mem_mgr(0);
+    if(!mem_mgr) mem_mgr=&def_mem_mgr;
+    T* A=(T*)mem_mgr->malloc(n_elem, sizeof(T));
+
+    if(!TypeTraits<T>::IsPOD()){ // Call constructors
+#pragma omp parallel for
+      for(size_t i=0;i<n_elem;i++){
+      T* Ai=new(A+i) T();
+      assert(Ai==(A+i));
+    }
+    }else{
+#ifndef NDEBUG
+#pragma omp parallel for
+      for(size_t i=0;i<n_elem*sizeof(T);i++){
+      ((char*)A)[i]=0;
+    }
+#endif
+    }
+
+    assert(A);
+    return A;
+  }
 
   template <class T>
-  inline void aligned_delete(T* A, const MemoryManager* mem_mgr=&glbMemMgr);
+  inline void aligned_delete(T* A, const MemoryManager* mem_mgr=&glbMemMgr){
+    if (!A) return;
+    if(!TypeTraits<T>::IsPOD()){ // Call destructors
+      MemoryManager::MemHead* mem_head=MemoryManager::GetMemHead(A);
+      size_t type_size=mem_head->type_size;
+      size_t n_elem=mem_head->n_elem;
+      for(size_t i=0;i<n_elem;i++){
+	((T*)(((char*)A)+i*type_size))->~T();
+      }
+    }else{
+#ifndef NDEBUG
+      MemoryManager::MemHead* mem_head=MemoryManager::GetMemHead(A);
+      size_t type_size=mem_head->type_size;
+      size_t n_elem=mem_head->n_elem;
+      size_t size=n_elem*type_size;
+#pragma omp parallel for
+      for(size_t i=0;i<size;i++){
+	((char*)A)[i]=0;
+      }
+#endif
+    }
 
-  inline void * memcopy(void * destination, const void * source, size_t num);
+    static MemoryManager def_mem_mgr(0);
+    if(!mem_mgr) mem_mgr=&def_mem_mgr;
+    mem_mgr->free(A);
+  }
+
+  inline void* memcopy( void * destination, const void * source, size_t num){
+    if(destination==source || num==0) return destination;
+    return memcpy ( destination, source, num );
+  }
 
 }//end namespace
 }//end namespace
-
-#include <mem_mgr.txx>
 
 #endif //_PVFMM_MEM_MGR_HPP_
