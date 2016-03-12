803683a559a82eecf0c4bfb2f5ae812b8e9e1f4b
Rio Yokota <rioyokota@gmail.com>
2016-03-12 11:53:02 +0300

    Merge headers: Step 101.

--- pvfmm/include/types.hpp
+++ pvfmm/include/types.hpp
@@ -12,7 +12,7 @@
 #define MAX_DEPTH 62
 #define MEM_ALIGN 64
 #define DEVICE_BUFFER_SIZE 1024LL
-#define CACHE_SIZE 64
+#define BLOCK_SIZE 64
 
 #define UNUSED(x) (void)(x) // to ignore unused variable warning.
 
