eaf2a80f8ebc69b5778cf02e18d5ef5864e05a2e
Rio Yokota <rioyokota@gmail.com>
2016-03-22 21:34:15 +0800

    Added step5.c

--- fmm/step5.c
+++ fmm/step5.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -90,10 +91,17 @@ int main() {
   }
   int ic = index[0], ncells = 1;
   int offset[N];
+  int leafs = 1<<(2*level);
+  int map[leafs];
+  for (i=0; i<leafs; i++) {
+    map[i] = -1;
+  }
   offset[0] = 0;
+  map[ic] = 0;
   for (i=0; i<N; i++) {
     if (ic != index[i]) {
       offset[ncells] = i;
+      map[index[i]] = ncells;
       ic = index[i];
       ncells++;
     }
@@ -106,13 +114,22 @@ int main() {
     maxcells += 1 << (2*i);
     levelOffset[i+1] = maxcells;
   }
-  double *Multipole = (double*)malloc(maxcells*sizeof(double));
+  double Multipole[maxcells];
+  double Local[maxcells];
+  for (i=0; i<maxcells; i++) {
+    Multipole[i] = 0;
+    Local[i] = 0;
+  }
+  printf("P2M\n");
   // P2M
-  for (i=0; i<ncells; i++) {
-    for (j=offset[i]; j<offset[i+1]; j++) {
-      Multipole[index[j]+levelOffset[level]] += q[j];
+  for (i=0; i<leafs; i++) {
+    if (map[i] >=0) {
+      for (j=offset[map[i]]; j<offset[map[i]+1]; j++) {
+	Multipole[index[j]+levelOffset[level]] += q[j];
+      }
     }
   }
+  printf("M2M\n");
   // M2M
   int l;
   for (l=level; l>0; l--) {
@@ -122,8 +139,8 @@ int main() {
       Multipole[ip] += Multipole[i+levelOffset[l]];
     }
   }
+  printf("M2L\n");
   // M2L
-  double *Local = (double*)malloc(maxcells*sizeof(double));
   for (l=2; l<=level; l++) {
     int nc = 1 << (2*l);
     for (i=0; i<nc; i++) {
@@ -137,18 +154,87 @@ int main() {
       if(iX[1]==(1<<(l-1))-1) iymax = 0;
       for (ix=ixmin; ix<=ixmax; ix++) {
 	for (iy=iymin; iy<=iymax; iy++) {
-	  int iX2[2];
+	  int iX2[2],jX[2];
 	  iX2[0] = iX[0]+ix;
 	  iX2[1] = iX[1]+iy;
-	  getIndex(&ic,iX2,3);
-	  for (j=0; j<4; j++) {
-	    ic*4+j;
+	  getIndex(&ic, iX2, l-1);
+	  int jc;
+	  for (jc=0; jc<4; jc++) {
+	    getIX(ic*4+jc,jX);
+	    if(abs(iX[0]-jX[0])>1||abs(iX[1]-jX[1])>1) {
+	      double dx = (iX[0] - jX[0] + 0.0) / (1 << l);
+	      double dy = (iX[1] - jX[1] + 0.0) / (1 << l);
+	      double r = sqrt(dx*dx+dy*dy);
+	      getIndex(&j, jX, l);
+	      Local[i+levelOffset[l]] += Multipole[j+levelOffset[l]] / r;
+	    }
 	  }
 	}
       }
     }
   }
-  free(Multipole);
-  free(Local);
+  printf("L2L\n");
+  // L2L
+  for (l=2; l<=level; l++) {
+    int nc = 1 << (2*l);
+    for (i=0; i<nc; i++) {
+      int ip = i/4 + levelOffset[l-1];
+      Local[i+levelOffset[l]] += Local[ip];
+    }
+  }
+  printf("L2P\n");
+  // L2P
+  for (i=0; i<leafs; i++) {
+    if (map[i] >=0) {
+      for (j=offset[map[i]]; j<offset[map[i]+1]; j++) {
+	u[j] += Local[index[j]+levelOffset[level]];
+      }
+    }
+  }
+  printf("P2P\n");
+  // P2P
+  for (ic=0; ic<leafs; ic++) {
+    if (map[ic] >= 0) {
+      getIX(ic,iX);
+      int ix, iy;
+      int ixmin = -1, iymin = -1;
+      int ixmax =  1, iymax =  1;
+      if(iX[0]==0) ixmin = 0;
+      if(iX[1]==0) iymin = 0;
+      if(iX[0]==(1<<level)-1) ixmax = 0;
+      if(iX[1]==(1<<level)-1) iymax = 0;
+      for (ix=ixmin; ix<=ixmax; ix++) {
+	for (iy=iymin; iy<=iymax; iy++) {
+	  int iX2[2],jX[2];
+	  iX2[0] = iX[0]+ix;
+	  iX2[1] = iX[1]+iy;
+	  int jc;
+	  if (map[jc] >=0) {
+	    getIndex(&jc,iX2,level);
+	    for (i=offset[map[ic]]; i<offset[map[ic]+1]; i++) {
+	      for (j=offset[map[jc]]; j<offset[map[jc]+1]; j++) {
+		double dx = x[i] - x[j];
+		double dy = y[i] - y[j];
+		double r = sqrt(dx*dx+dy*dy);
+		if(r!=0) u[i] += q[j] / r;
+	      }
+	    }
+	  }
+	}
+      }
+    }
+  }
+  // Check answer
+  printf("Direct\n");
+  for (i=0; i<N; i++) {
+    double ud = 0;
+    for (j=0; j<N; j++) {
+      double dx = x[i] - x[j];
+      double dy = y[i] - y[j];
+      double r = sqrt(dx*dx+dy*dy);
+      if(r!=0) ud += q[j] / r;
+    }
+    printf("%lf %lf\n",u[i],ud);
+  }
 }
 
