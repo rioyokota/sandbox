afe2405f95019a2df66235c91f3359b3c9b6cb3a
Rio Yokota <rioyokota@gmail.com>
2016-09-06 11:19:35 +0900

    Added OpenCL vmul

--- opencl/vadd.cxx
+++ opencl/vadd.cxx
@@ -10,95 +10,95 @@
 #define MEM_SIZE (128)
 #define MAX_SOURCE_SIZE (0x100000)
 
-int main()
-{
-    cl_platform_id platform_id = NULL;
-    cl_device_id device_id = NULL;
-    cl_context context = NULL;
-    cl_command_queue command_queue = NULL;
-    cl_mem memobj = NULL;
-    cl_program program = NULL;
-    cl_kernel kernel = NULL;
-    cl_uint ret_num_devices;
-    cl_uint ret_num_platforms;
-    cl_int ret;
+int main() {
+  cl_platform_id platform_id = NULL;
+  cl_device_id device_id = NULL;
+  cl_context context = NULL;
+  cl_command_queue command_queue = NULL;
+  cl_mem d_a = NULL;
+  cl_program program = NULL;
+  cl_kernel kernel = NULL;
+  cl_uint ret_num_devices;
+  cl_uint ret_num_platforms;
+  cl_int ret;
 
-    float mem[MEM_SIZE];
+  float * h_a = (float *) malloc(MEM_SIZE*sizeof(float));
 
-    FILE *fp;
-    const char fileName[] = "./vadd.cl";
-    size_t source_size;
-    char *source_str;
-    cl_int i;
+  FILE *fp;
+  const char fileName[] = "./vadd.cl";
+  size_t source_size;
+  char *source_str;
+  cl_int i;
 
-    /* カーネルを含むソースコードをロード */
-    fp = fopen(fileName, "r");
-    if (!fp) {
-        fprintf(stderr, "Failed to load kernel.\n");
-        exit(1);
-    }
-    source_str = (char *)malloc(MAX_SOURCE_SIZE);
-    source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp );
-    fclose( fp );
+  /* カーネルを含むソースコードをロード */
+  fp = fopen(fileName, "r");
+  if (!fp) {
+    fprintf(stderr, "Failed to load kernel.\n");
+    exit(1);
+  }
+  source_str = (char *)malloc(MAX_SOURCE_SIZE);
+  source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp );
+  fclose( fp );
 
-    /* データを初期化 */
-    for( i = 0; i < MEM_SIZE; i++ ) {
-        mem[i] = i;
-    }
+  /* データを初期化 */
+  for( i = 0; i < MEM_SIZE; i++ ) {
+    h_a[i] = i;
+  }
 
-    /* プラットフォーム・デバイスの情報の取得 */
-    ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+  /* プラットフォーム・デバイスの情報の取得 */
+  ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+  ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
 
-    /* OpenCLコンテキストの作成 */
-    context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+  /* OpenCLコンテキストの作成 */
+  context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
-    /* コマンドキューの作成 */
-    command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+  /* コマンドキューの作成 */
+  command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
-    /* メモリバッファの作成 */
-    memobj = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(float), NULL, &ret);
+  /* メモリバッファの作成 */
+  d_a = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(float), NULL, &ret);
 
-    /* メモリバッファにデータを転送 */
-    ret = clEnqueueWriteBuffer(command_queue, memobj, CL_TRUE, 0, MEM_SIZE * sizeof(float), mem, 0, NULL, NULL);
+  /* メモリバッファにデータを転送 */
+  ret = clEnqueueWriteBuffer(command_queue, d_a, CL_TRUE, 0, MEM_SIZE * sizeof(float), h_a, 0, NULL, NULL);
 
-    /* 読み込んだソースからカーネルプログラムを作成 */
-    program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
+  /* 読み込んだソースからカーネルプログラムを作成 */
+  program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
 
-    /* カーネルプログラムをビルド */
-    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+  /* カーネルプログラムをビルド */
+  ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
-    /* OpenCLカーネルの作成 */
-    kernel = clCreateKernel(program, "vecAdd", &ret);
+  /* OpenCLカーネルの作成 */
+  kernel = clCreateKernel(program, "vecAdd", &ret);
 
-    /* OpenCLカーネル引数の設定 */
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobj);
+  /* OpenCLカーネル引数の設定 */
+  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_a);
 
-    size_t global_work_size[3] = {MEM_SIZE, 0, 0};
-    size_t local_work_size[3]  = {MEM_SIZE, 0, 0};
+  size_t global_work_size[3] = {MEM_SIZE, 0, 0};
+  size_t local_work_size[3]  = {MEM_SIZE, 0, 0};
 
-    /* OpenCLカーネルを実行 */
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+  /* OpenCLカーネルを実行 */
+  ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
 
-    /* メモリバッファから結果を取得 */
-    ret = clEnqueueReadBuffer(command_queue, memobj, CL_TRUE, 0, MEM_SIZE * sizeof(float), mem, 0, NULL, NULL);
+  /* メモリバッファから結果を取得 */
+  ret = clEnqueueReadBuffer(command_queue, d_a, CL_TRUE, 0, MEM_SIZE * sizeof(float), h_a, 0, NULL, NULL);
 
-    /* 結果の表示 */
-    for(i=0; i<MEM_SIZE; i++) {
-        printf("mem[%d] : %f\n", i, mem[i]);
-    }
+  /* 結果の表示 */
+  for(i=0; i<MEM_SIZE; i++) {
+    printf("mem[%d] : %f\n", i, h_a[i]);
+  }
 
-    /* 終了処理 */
-    ret = clFlush(command_queue);
-    ret = clFinish(command_queue);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseProgram(program);
-    ret = clReleaseMemObject(memobj);
-    ret = clReleaseCommandQueue(command_queue);
-    ret = clReleaseContext(context);
+  /* 終了処理 */
+  ret = clFlush(command_queue);
+  ret = clFinish(command_queue);
+  ret = clReleaseKernel(kernel);
+  ret = clReleaseProgram(program);
+  ret = clReleaseMemObject(d_a);
+  ret = clReleaseCommandQueue(command_queue);
+  ret = clReleaseContext(context);
 
-    free(source_str);
+  free(h_a);
+  free(source_str);
 
-    return 0;
+  return 0;
 }
 
---
+++ opencl/vmul.cxx
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <CL/cl.h>
+
+#define DATA_SIZE (1024*1240)
+
+const char *KernelSource = "\n"      \
+  "__kernel void square(                    \n" \
+  "   __global float* input,                \n" \
+  "   __global float* output,               \n" \
+  "   const unsigned int count)             \n" \
+  "{                                        \n" \
+  "   int i = get_global_id(0);             \n" \
+  "   if(i < count)                         \n" \
+  "       output[i] = input[i] * input[i];  \n" \
+  "}                                        \n" \
+  "\n";
+
+int main(int argc, char* argv[])
+{
+  int devType=CL_DEVICE_TYPE_CPU;
+  
+  cl_int err;
+  
+  size_t global;
+  size_t local;
+  
+  cl_platform_id platform_id;
+  cl_device_id device_id;
+  cl_context context;
+  cl_command_queue command_queue; // compute command queue
+  cl_program program;        // compute program
+  cl_kernel kernel;          // compute kernel
+  
+  // Connect to a compute device
+  err = clGetPlatformIDs(1, &platform_id, NULL);
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to find a platform!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Get a device of the appropriate type
+  err = clGetDeviceIDs(platform_id, devType, 1, &device_id, NULL);
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to create a device group!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Create a compute context
+  context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+  if (!context) {
+    std::cerr << "Error: Failed to create a compute context!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Create a command command_queue
+  command_queue = clCreateCommandQueue(context, device_id, 0, &err);
+  if (!command_queue) {
+    std::cerr << "Error: Failed to create a command command_queue!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Create the compute program from the source buffer
+  program = clCreateProgramWithSource(context, 1, 
+                                      (const char **) &KernelSource,
+                                      NULL, &err);
+  if (!program) {
+    std::cerr << "Error: Failed to create compute program!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Build the program executable
+  err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+  if (err != CL_SUCCESS) {
+    size_t len;
+    char buffer[2048];
+    
+    std::cerr << "Error: Failed to build program executable!" << std::endl;
+    clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG,
+                          sizeof(buffer), buffer, &len);
+    std::cerr << buffer << std::endl;
+    exit(1);
+  }
+  
+  // Create the compute kernel in the program
+  kernel = clCreateKernel(program, "square", &err);
+  if (!kernel || err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to create compute kernel!" << std::endl;
+    exit(1);
+  }
+  
+  // create data for the run
+  float* data = new float[DATA_SIZE];    // original data set given to device
+  float* results = new float[DATA_SIZE]; // results returned from device
+  unsigned int correct;               // number of correct results returned
+  cl_mem input;                       // device memory used for the input array
+  cl_mem output;                      // device memory used for the output array
+  
+  // Fill the vector with random float values
+  int count = DATA_SIZE;
+  for(int i = 0; i < count; i++)
+    data[i] = rand() / (float)RAND_MAX;
+  
+  // Create the device memory vectors
+  //
+  input = clCreateBuffer(context,  CL_MEM_READ_ONLY,  
+                         sizeof(float) * count, NULL, NULL);
+  output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+                          sizeof(float) * count, NULL, NULL);
+  if (!input || !output) {
+    std::cerr << "Error: Failed to allocate device memory!" << std::endl;
+    exit(1);
+  }   
+  
+  // Transfer the input vector into device memory
+  err = clEnqueueWriteBuffer(command_queue, input, 
+                             CL_TRUE, 0, sizeof(float) * count, 
+                             data, 0, NULL, NULL);
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to write to source array!" << std::endl;
+    exit(1);
+  }
+  
+  // Set the arguments to the compute kernel
+  err = 0;
+  err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
+  err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
+  err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &count);
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to set kernel arguments! " << err << std::endl;
+    exit(1);
+  }
+  
+  // Get the maximum work group size for executing the kernel on the device
+  err = clGetKernelWorkGroupInfo(kernel, device_id, 
+                                 CL_KERNEL_WORK_GROUP_SIZE, 
+                                 sizeof(local), &local, NULL);
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to retrieve kernel work group info! "
+         <<  err << std::endl;
+    exit(1);
+  }
+  
+  // Execute the kernel over the vector using the 
+  // maximum number of work group items for this device
+  global = count;
+  err = clEnqueueNDRangeKernel(command_queue, kernel, 
+                               1, NULL, &global, &local, 
+                               0, NULL, NULL);
+  if (err) {
+    std::cerr << "Error: Failed to execute kernel!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Wait for all command_queue to complete
+  clFinish(command_queue);
+  
+  // Read back the results from the device to verify the output
+  //
+  err = clEnqueueReadBuffer( command_queue, output,
+                             CL_TRUE, 0, sizeof(float) * count,
+                             results, 0, NULL, NULL ); 
+  if (err != CL_SUCCESS) {
+    std::cerr << "Error: Failed to read output array! " <<  err << std::endl;
+    exit(1);
+  }
+  
+  // Validate our results
+  //
+  correct = 0;
+  for(int i = 0; i < count; i++) {
+    if(results[i] == data[i] * data[i])
+      correct++;
+  }
+  
+  // Print a brief summary detailing the results
+  std::cout << "Computed " << correct << "/" << count << " correct values" << std::endl;
+  std::cout << "Computed " << 100.f * (float)correct/(float)count
+       << "% correct values" << std::endl;
+  
+  // Shutdown and cleanup
+  delete [] data; delete [] results;
+  
+  clReleaseMemObject(input);
+  clReleaseMemObject(output);
+  clReleaseProgram(program);
+  clReleaseKernel(kernel);
+  clReleaseCommandQueue(command_queue);
+  clReleaseContext(context);
+  
+  return 0;
+}
