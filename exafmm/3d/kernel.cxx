a2956288673b6516d6f95b4e035237a7f1605475
Rio Yokota <rioyokota@gmail.com>
2017-03-14 16:40:55 +0900

    Removed unused headers

--- exafmm/3d/kernel.cxx
+++ exafmm/3d/kernel.cxx
@@ -17,14 +17,7 @@ int main(int argc, char ** argv) {
   Cells cells(4);
   Verify verify;
   jbodies[0].X = 2;
-#if EXAFMM_BIOTSAVART
-  jbodies[0].SRC[0] = drand48();
-  jbodies[0].SRC[1] = drand48();
-  jbodies[0].SRC[2] = drand48();
-  jbodies[0].SRC[3] = 0.1;
-#else
   jbodies[0].SRC = 1;
-#endif
   C_iter Cj = cells.begin();
   Cj->X = 1;
   Cj->X[0] = 3;
@@ -34,7 +27,6 @@ int main(int argc, char ** argv) {
   Cj->M.resize(kernel.NTERM, 0.0);
   kernel.P2M(Cj);
 
-#if 1
   C_iter CJ = cells.begin()+1;
   CJ->ICHILD = Cj-cells.begin();
   CJ->NCHILD = 1;
@@ -58,14 +50,6 @@ int main(int argc, char ** argv) {
   Ci->IPARENT = 2;
   Ci->L.resize(kernel.NTERM, 0.0);
   kernel.L2L(Ci, cells.begin());
-#else
-  C_iter Ci = cells.begin()+3;
-  Ci->X = 1;
-  Ci->X[0] = -3;
-  Ci->R = 1;
-  Ci->L.resize(kernel.NTERM, 0.0);
-  kernel.M2L(Ci, Cj);
-#endif
 
   bodies[0].X = 2;
   bodies[0].X[0] = -2;
@@ -74,7 +58,6 @@ int main(int argc, char ** argv) {
   Ci->BODY = bodies.begin();
   Ci->NBODY = bodies.size();
   kernel.L2P(Ci);
-
 
   for (B_iter B=bodies2.begin(); B!=bodies2.end(); B++) {
     *B = bodies[B-bodies2.begin()];
@@ -89,7 +72,6 @@ int main(int argc, char ** argv) {
   }
 
   std::fstream file;
-  file.open("kernel.dat", std::ios::out | std::ios::app);
   double potDif = verify.getDifScalar(bodies, bodies2);
   double potNrm = verify.getNrmScalar(bodies);
   double accDif = verify.getDifVector(bodies, bodies2);
@@ -99,7 +81,5 @@ int main(int argc, char ** argv) {
   double accRel = std::sqrt(accDif/accNrm);
   verify.print("Rel. L2 Error (pot)",potRel);
   verify.print("Rel. L2 Error (acc)",accRel);
-  file << args.P << " " << std::sqrt(potDif/potNrm) << "  " << std::sqrt(accDif/accNrm) << std::endl;
-  file.close();
   return 0;
 }
