52bb36ef61fad434bfbd24880d6fc20cfd456d7f
Rio Yokota <rioyokota@gmail.com>
2017-04-23 20:36:43 +0900

    Update step01

--- 12steps/openmp/step01.cpp
+++ 12steps/openmp/step01.cpp
@@ -1,17 +1,19 @@
 #include <cstdlib>
 #include <cstdio>
-#include <ctime>
+#include <sys/time.h>
+
 int main(int argc, char ** argv) {
+  struct timeval tic, toc;
   int n = atoi(argv[1]);
   double * a = new double [n];
   double * b = new double [n];
-  std::clock_t tic = std::clock();
+  gettimeofday(&tic, NULL);
 #pragma omp parallel for
   for (int i=1; i<n; i++) {
     b[i] = (a[i] + a[i-1]) / 2.0;
   }
-  std::clock_t toc = std::clock();
-  printf("%lf s\n",(toc-tic)/double(CLOCKS_PER_SEC));
+  gettimeofday(&toc, NULL);
+  printf("%lf s\n",toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6);
   delete[] a;
   delete[] b;
 }
