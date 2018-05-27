1f2988d7d95dc04d5ca0d10e84c9ad2df191dd79
Rio Yokota <rioyokota@gmail.com>
2018-05-28 07:33:56 +0900

    kernel call

--- cutlass/gemm/dispatch.h
+++ cutlass/gemm/dispatch.h
@@ -1,42 +1,7 @@
-/******************************************************************************
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the NVIDIA CORPORATION nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
-
 #pragma once
-
-/**
- * \file
- * GEMM kernel entrypoint and dispatch stub
- */
-
 #include <stdint.h>
-
 #include "../util/util.h"
 #include "block_task.h"
-#include "block_task_wmma.h"
 #include "grid_raster.h"
 #include "dispatch_policies.h"
 #include "k_split_control.h"
@@ -44,126 +9,6 @@
 namespace cutlass {
 namespace gemm {
 
-
-/******************************************************************************
- * param_pack
- ******************************************************************************/
-
-/**
- * Parameter-pack structure
- *
- * Kernel launch latency is reduced when kernel arguments are wrapped into
- * a single parameter
- */
-template <
-    typename value_t,
-    typename accum_t,
-    typename epilogue_op_t>
-struct param_pack
-{
-    int m;                      ///< Height in rows of op(A) and C
-    int n;                      ///< Width in columns of op(B) and C
-    int k;                      ///< Width in columns of op(A) and height in rows of op(B)
-    k_split_control k_split;    ///< Abstraction for controlling inter-block k-splitting
-    value_t *d_a;               ///< Pointer to matrix A array values
-    value_t *d_b;               ///< Pointer to matrix B array values
-    accum_t *d_c;               ///< Pointer to matrix C array values
-    epilogue_op_t epilogue_op;
-
-    param_pack(
-        int m,                      ///< Height in rows of op(A) and C
-        int n,                      ///< Width in columns of op(B) and C
-        int k,                      ///< Width in columns of op(A) and height in rows of op(B)
-        k_split_control k_split,    ///< Abstraction for controlling inter-block k-splitting
-        epilogue_op_t op,           ///< Epilogue operation to update matrix C
-        value_t *d_a,               ///< Pointer to matrix A array values
-        value_t *d_b,               ///< Pointer to matrix B array values
-        accum_t *d_c)               ///< Pointer to matrix C array values
-    :
-        m(m),
-        n(n),
-        k(k),
-        k_split(k_split),
-        epilogue_op(op),
-        d_a(d_a),
-        d_b(d_b),
-        d_c(d_c)
-    {}
-
-};
-
-
-/******************************************************************************
- * Conditionally select the appropriate GEMM threadblock task
- ******************************************************************************/
-
-/// Conditional selection for block task
-template <
-    math_operation_class_t      math_op,            ///<
-    typename                    block_task_policy_t,  ///< Parameterization of block_task_policy
-    typename                    value_t,            ///< Multiplicand value type (matrices A and B)
-    typename                    accum_t,            ///< Accumulator value type (matrix C and scalars)
-    matrix_transform_t::kind_t  TransformA,         ///< View transform enumerant for matrix A
-    int                         LdgAlignA,          ///< Alignment (in bytes) for A operand
-    matrix_transform_t::kind_t  TransformB,         ///< View transform enumerant for matrix B
-    int                         LdgAlignB,          ///< Alignment (in bytes) for B operand
-    typename                    epilogue_op_t,      ///< Epilogue operation applied to GEMM
-    int                         LdgAlignC,          ///< Alignment (in bytes) for C operand
-    bool                        AllowRaggedTiles    ///< Whether GEMM supports matrix sizes other than multiple of BlockItems{XY}
->
-struct gemm_block_task;
-
-/// Scalar math operations
-template <
-    typename                    block_task_policy_t,  ///< Parameterization of block_task_policy
-    typename                    value_t,            ///< Multiplicand value type (matrices A and B)
-    typename                    accum_t,            ///< Accumulator value type (matrix C and scalars)
-    matrix_transform_t::kind_t  TransformA,         ///< View transform enumerant for matrix A
-    int                         LdgAlignA,          ///< Alignment (in bytes) for A operand
-    matrix_transform_t::kind_t  TransformB,         ///< View transform enumerant for matrix B
-    int                         LdgAlignB,          ///< Alignment (in bytes) for B operand
-    typename                    epilogue_op_t,      ///< Epilogue operation applied to GEMM
-    int                         LdgAlignC,          ///< Alignment (in bytes) for C operand
-    bool                        AllowRaggedTiles    ///< Whether GEMM supports matrix sizes other than multiple of BlockItems{XY}
->
-struct gemm_block_task<
-    math_operation_class_t::scalar,
-    block_task_policy_t,
-    value_t,
-    accum_t,
-    TransformA,
-    LdgAlignA,
-    TransformB,
-    LdgAlignB,
-    epilogue_op_t,
-    LdgAlignC,
-    AllowRaggedTiles
->
-{
-    // Parameterize task type
-    typedef block_task<
-            block_task_policy_t,
-            value_t,
-            accum_t,
-            TransformA,
-            LdgAlignA,
-            TransformB,
-            LdgAlignB,
-            epilogue_op_t,
-            LdgAlignC,
-            AllowRaggedTiles> type;
-};
-
-/******************************************************************************
- * GEMM kernel entrypoint
- ******************************************************************************/
-
-/**
- * GEMM kernel
- *
- * NB: Not sure why NVVM is doing stuff with "__launch_bounds__" instead of just
- * passing it along to PTXAS, but it is currently resulting in less optimal codegen
- */
 template <
     matrix_transform_t::kind_t  TransformA,         ///< Transformation op for matrix A
     int                         LdgAlignA,          ///< Alignment of A matrix elements in bytes
@@ -288,11 +133,11 @@ struct launch_configuration
  */
 template <
   typename                    epilogue_op_t,
-    int                         LdgAlignC,          ///< Alignment of C matrix elements in bytes
-    bool                        AllowRaggedTiles,   ///< Boolean to indicate whether AllowRaggedTiles handling is enabled
-    typename                    kernel_ptr_t>       ///< GEMM kernel function pointer type
+  int                         LdgAlignC,          ///< Alignment of C matrix elements in bytes
+  bool                        AllowRaggedTiles,
+  typename                    kernel_ptr_t>       ///< GEMM kernel function pointer type
 launch_configuration dispatch(
-    kernel_ptr_t    kernel_ptr,                     ///< GEMM kernel function pointer
+                              kernel_ptr_t    kernel_ptr,
     int             m,                              ///< Height in rows of op(A) and C
     int             n,                              ///< Width in columns of op(B) and C
     int             k,                              ///< Width in columns of op(A) and height in rows of op(B)
@@ -342,15 +187,28 @@ launch_configuration dispatch(
                           config.block,
                           config.grid);
   config.split_k = k_split.split_k;
-  kernel_ptr<<< config.grid, config.block, dynamic_smem_bytes, stream >>>(
-                                                                          m,
-                                                                          n,
-                                                                          k,
-                                                                          k_split,
-                                                                          epilogue,
-                                                                          d_a,
-                                                                          d_b,
-                                                                          d_c);
+  gemm::kernel<
+    TransformA,
+    16,
+    TransformB,
+    16,
+    float,
+    float,
+    epilogue_op_t,
+    4,
+    AllowRaggedTiles>
+    <<< config.grid,
+    config.block,
+    dynamic_smem_bytes,
+    stream >>>(
+               m,
+               n,
+               k,
+               k_split,
+               epilogue,
+               d_a,
+               d_b,
+               d_c);
   return config;
 }
 
