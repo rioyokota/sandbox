991eed055dabb9486a06a10abe3e88bc746049d9
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-09-15 19:35:10 +0900

    socket bugfix

--- socket/udp_server.c
+++ socket/udp_server.c
@@ -16,7 +16,7 @@ int main() {
   servaddr.sin_port = htons(PORT);
   servaddr.sin_addr.s_addr = INADDR_ANY;
   bind(s, (struct sockaddr *)&servaddr, sizeof(servaddr));
-  n = recvfrom(s, buf, strlen(buf), MSG_WAITALL, (struct sockaddr *)&recvaddr, &l);
+  n = recvfrom(s, buf, sizeof(buf), MSG_WAITALL, (struct sockaddr *)&recvaddr, &l);
   buf[n] = '\0';
   printf("%s\n", buf);
   return 0;
