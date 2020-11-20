51d3f8d05b056c5626dffd15320f3a089cf2c3a8
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-11-20 21:39:30 +0900

    mpi_direct for burn

--- gpu/mpi_direct.cu
+++ gpu/mpi_direct.cu
@@ -71,8 +71,8 @@ int main(int argc, char **argv) {
   }
 
   // Initialize
-  int N = 1 << 16;
-  //int N = 1 << 24;
+  //int N = 1 << 16;
+  int N = 1 << 24;
   real_t OPS = 20. * N * N * 1e-9;
   real_t EPS2 = 1e-6;
   double tic, toc;
@@ -107,7 +107,7 @@ int main(int argc, char **argv) {
   cudaMemcpy(y_d, y, N * sizeof(real_t), cudaMemcpyHostToDevice);
   cudaMemcpy(z_d, z, N * sizeof(real_t), cudaMemcpyHostToDevice);
   cudaMemcpy(m_d, m, N * sizeof(real_t), cudaMemcpyHostToDevice);
-  for (int i=0; i<20; i++) {
+  for (int i=0; i<100; i++) {
     tic = get_time();
     GPUkernel<<<N/THREADS,THREADS>>>(N, x_d, y_d, z_d, m_d, p_d, ax_d, ay_d, az_d, EPS2);
     toc = get_time();
