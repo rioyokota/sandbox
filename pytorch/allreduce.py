827d0dd810e6af5cfe6187a20fad0a6152c8ecc3
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-12-29 16:36:40 +0900

    Change function name to get_rank and get_world_size

--- pytorch/allreduce.py
+++ pytorch/allreduce.py
@@ -2,14 +2,14 @@ import os
 import torch
 import torch.distributed as dist
 
-def get_mpi_rank():
+def get_rank():
     rank = '0'
     rank = os.getenv('PMI_RANK', rank)
     rank = os.getenv('OMPI_COMM_WORLD_RANK', rank)
     rank = os.getenv('MV2_COMM_WORLD_RANK', rank)
     return int(rank)
 
-def get_mpi_size():
+def get_world_size():
     size = '1'
     size = os.getenv('PMI_SIZE', size)
     size = os.getenv('OMPI_COMM_WORLD_SIZE', size)
@@ -19,8 +19,8 @@ def get_mpi_size():
 master_addr = os.getenv("MASTER_ADDR", default="localhost")
 master_port = os.getenv('MASTER_PORT', default='8888')
 method = "tcp://{}:{}".format(master_addr, master_port)
-rank = get_mpi_rank()
-world_size = get_mpi_size()
+rank = get_rank()
+world_size = get_world_size()
 dist.init_process_group("nccl", init_method=method, rank=rank, world_size=world_size)
 print('Rank: {}, Size: {}, Host: {}'.format(dist.get_rank(), dist.get_world_size(), master_addr))
 ngpus = torch.cuda.device_count()
