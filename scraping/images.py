aa9c0f573e0a0cb9d13aead0293d9aebe02851d3
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-01-22 21:32:37 +0900

    Print counter

--- scraping/images.py
+++ scraping/images.py
@@ -12,14 +12,16 @@ def download(url, pathname):
 
 def main():
     url_file = '2021-01-01.tsv.gz'
-    save_path = 'data/data/'
+    save_path = '/groups1/gcc50533/data/twitter/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+    f = pd.read_csv(url_file,header=None,sep='\t')
+    total = len(f)
     df = pd.read_csv(url_file,header=None,sep='\t',chunksize=1,nrows=10)
-    for line in df:
+    for count, line in enumerate(df):
         if line.values[0][6] != 'グランブルー ファンタジー':
             url = line.values[0][3]
-            print(line.values[0])
+            print(f'{count} / {total}')
             download(url,save_path)
 
 if __name__ == '__main__':
