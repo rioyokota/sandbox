6c76d297e04d34ae37cfe83609fa8c4b679d78a8
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-01-22 21:37:20 +0900

    Add timer

--- scraping/images.py
+++ scraping/images.py
@@ -1,6 +1,7 @@
+import os
 import pandas as pd
 import requests
-import os
+from timeit import default_timer as timer
 
 def download(url, pathname):
     data = requests.get(url).content
@@ -12,17 +13,20 @@ def download(url, pathname):
 
 def main():
     url_file = '2021-01-01.tsv.gz'
-    save_path = '/groups1/gcc50533/data/twitter/'
+    #save_path = '/groups1/gcc50533/data/twitter/'
+    save_path = 'data/'
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     f = pd.read_csv(url_file,header=None,sep='\t')
     total = len(f)
     df = pd.read_csv(url_file,header=None,sep='\t',chunksize=1,nrows=10)
+    tic = timer()
     for count, line in enumerate(df):
         if line.values[0][6] != 'グランブルー ファンタジー':
             url = line.values[0][3]
-            print(f'{count} / {total}')
             download(url,save_path)
+            toc = timer()
+            print(f'{count} / {total} : {toc-tic} s')
 
 if __name__ == '__main__':
     main()
