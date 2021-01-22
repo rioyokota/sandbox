488c83ea398743d02ae3f76d355678431b9a3d6a
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-01-22 20:45:05 +0900

    Scraper ready.

--- scraping/images.py
+++ scraping/images.py
@@ -1,5 +1,22 @@
+import pandas as pd
 import requests
 import os
-from tqdm import tqdm
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urljoin, urlparse
+
+def download(url, pathname):
+    data = requests.get(url).content
+    filename = os.path.join(pathname, url.split("/")[-1])
+    with open(filename, "wb") as f:
+        f.write(data)
+    if os.path.getsize(filename) == 0:
+        os.remove(filename)
+
+def main():
+    df = pd.read_csv('2021-01-01.tsv.gz',header=None,sep='\t',chunksize=1,nrows=20)
+    for line in df:
+        if line.values[0][6] != 'グランブルー ファンタジー':
+            url = line.values[0][3]
+            print(line.values[0][0])
+            download(url,'data/')
+
+if __name__ == '__main__':
+    main()
