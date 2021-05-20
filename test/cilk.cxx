f8531feb240a61688b6cb3430abf18842cee3b35
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-05-20 14:09:27 +0900

    Remove unused code

--- test/cilk.cxx
+++
@@ -1,8 +0,0 @@
-#include <cilk/cilk.h>
-#include <cilk/cilk_api.h>
-#include <iostream>
-
-int main(int argc, char** argv) {
-  __cilkrts_set_param("nworkers",argv[1]);
-  std::cout << __cilkrts_get_nworkers() << std::endl;
-}
--- test/omp.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <omp.h>
-
-int main() {
-#pragma omp parallel
-  printf("%d\n",omp_get_num_threads());
-}
