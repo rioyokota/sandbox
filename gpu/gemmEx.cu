a79c75a204c58cbff3e5208edea94624bb61481e
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-06-16 17:17:02 +0900

    Get gemmEx to work.

--- gpu/gemmEx.cu
+++ gpu/gemmEx.cu
@@ -1,32 +1,62 @@
+#include <iostream>
+#include <typeinfo>
+#include <random>
+#include <stdint.h>
 #include <cublas_v2.h>
-#include <magma_v2.h>
+#include <chrono>
+using namespace std;
 
-int main(int argc, char **argv) {
-  int m = 128;
-  int n = 64;
-  int k = 32;
-  cublasStatus_t cublasStat = cublasCreate(&handle);
-  cublasStat = cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH);
-  size_t matrixSizeA = (size_t)m * k;
-  size_t matrixSizeB = (size_t)k * n;
-  size_t matrixSizeC = (size_t)m * n;
-  double **devPtrA = 0, **devPtrB = 0, **devPtrC = 0;
-  cudaMalloc((void**)&devPtrA[0], matrixSizeA * sizeof(devPtrA[0][0]));
-  cudaMalloc((void**)&devPtrB[0], matrixSizeB * sizeof(devPtrB[0][0]));
-  cudaMalloc((void**)&devPtrC[0], matrixSizeC * sizeof(devPtrC[0][0]));
-  double A  = (double *)malloc(matrixSizeA * sizeof(A[0]));
-  double B  = (double *)malloc(matrixSizeB * sizeof(B[0]));
-  double C  = (double *)malloc(matrixSizeC * sizeof(C[0]));
-  memset( A, 0xFF, matrixSizeA* sizeof(A[0]));
-  memset( B, 0xFF, matrixSizeB* sizeof(B[0]));
-  memset( C, 0xFF, matrixSizeC* sizeof(C[0]));
-  cublasSetMatrix(m, k, sizeof(A[0]), A, rowsA, devPtrA[i], rowsA);
-  cublasSetMatrix(k, n, sizeof(B[0]), B, rowsB, devPtrB[i], rowsB);
-  cublasSetMatrix(m, n, sizeof(C[0]), C, rowsC, devPtrC[i], rowsC);
-  double alpha = 1;
-  double beta = 1;
-  cublasStat = cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha,
-                            A, CUDA_R_16F, m, B, CUDA_R_16F, k,
-                            beta, C, CUDA_R_16F, m, CUDA_R_32F, algo);
-  return 0;
+int main(int argc, const char **argv) {
+  int m = 10240;
+  int k = 4096;
+  int n = 8192;
+  float alpha = 1.0;
+  float beta = 0.0;
+  int Nt = 10;
+  float *A, *B, *C;
+  cudaMallocManaged(&A, m * k * sizeof(float));
+  cudaMallocManaged(&B, k * n * sizeof(float));
+  cudaMallocManaged(&C, m * n * sizeof(float));
+  for (int i=0; i<m; i++)
+    for (int j=0; j<k; j++)
+      A[k*i+j] = drand48();
+  for (int i=0; i<k; i++)
+    for (int j=0; j<n; j++)
+      B[n*i+j] = drand48();
+  for (int i=0; i<n; i++)
+    for (int j=0; j<m; j++)
+      C[m*i+j] = 0;
+  cublasHandle_t cublas_handle;
+  cublasCreate(&cublas_handle);
+  auto tic = chrono::steady_clock::now();
+  for (int i = 0; i < Nt+2; i++) {
+    if (i == 2) tic = chrono::steady_clock::now();
+    cublasSgemmEx(cublas_handle,
+		CUBLAS_OP_N,
+		CUBLAS_OP_N,
+		m,
+		n,
+		k,
+		&alpha,
+		A,
+		CUDA_R_16F,
+		m,
+		B,
+		CUDA_R_16F,
+		k,
+		&beta,
+		C,
+		CUDA_R_32F,
+		m);
+    cudaDeviceSynchronize();
+  }
+  auto toc = chrono::steady_clock::now();
+  int64_t num_flops = (2 * int64_t(m) * int64_t(n) * int64_t(k)) + (2 * int64_t(m) * int64_t(n));
+  double tcublas = chrono::duration<double>(toc - tic).count() / Nt;
+  double cublas_flops = double(num_flops) / tcublas / 1.0e12;
+  printf("CUBLAS: %.2f TFlops\n", cublas_flops);
+  cudaFree(A);
+  cudaFree(B);
+  cudaFree(C);
+  cublasDestroy(cublas_handle);
 }
