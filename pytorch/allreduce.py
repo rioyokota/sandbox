8bc84c2b102a5aa648339c40a4be960da245f8a6
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-09-22 08:19:22 +0900

    Fixed hostname configuration in allreduce.py.

--- pytorch/allreduce.py
+++ pytorch/allreduce.py
@@ -16,16 +16,17 @@ def get_world_size():
     size = os.getenv('MV2_COMM_WORLD_SIZE', size)
     return int(size)
 
-master_addr = os.getenv("MASTER_ADDR", default="localhost")
+master_addr = os.getenv('MASTER_ADDR', default='localhost')
 master_port = os.getenv('MASTER_PORT', default='8888')
+hostname = os.getenv('HOSTNAME', default='localhost')
 method = "tcp://{}:{}".format(master_addr, master_port)
 rank = get_rank()
 world_size = get_world_size()
-dist.init_process_group("nccl", init_method=method, rank=rank, world_size=world_size)
-print('Rank: {}, Size: {}, Host: {}'.format(dist.get_rank(), dist.get_world_size(), master_addr))
 ngpus = torch.cuda.device_count()
+dist.init_process_group("nccl", init_method=method, rank=rank, world_size=world_size)
+print('Rank: {}, Size: {}, GPUs: {}, Master: {}, Host: {}'.format(dist.get_rank(), dist.get_world_size(), 
+                                                                  ngpus, master_addr, hostname))
 device = rank % ngpus
-
 model = torch.nn.Sequential(
     torch.nn.Linear(1,1,bias=False)
 ).to(device)
